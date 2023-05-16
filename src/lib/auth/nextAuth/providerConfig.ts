1cc6681ed7e4e7f9aeef6997939e672870319e8c
Kevin Bravo <bravokevinto@gmail.com>
2023-05-16 15:44:56 -0400

    feat: adding configuration for planetscale settings

--- src/lib/auth/nextAuth/providerConfig.ts
+++ src/lib/auth/nextAuth/providerConfig.ts
@@ -1,48 +1,40 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsPrivider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     session: {
         strategy: "jwt",
     },
-
     providers: [
         GoogleProvider({
             clientId: process.env.CLIENT_ID!,
             clientSecret: process.env.CLIENT_SECRET!,
         }),
-        // CredentialsPrivider({
-        //     name: "Credentials",
-        //     credentials: {
-        //         email: { label: "Email", type: "email", placeholder: "hola@gmail.com" },
-        //         password: { label: "Password", type: "password" }
-        //     },
-
-        //     async authorize(credentials) {
-        //         const user = { id: '1', name: 'hola', email: 'bravokevinto@gmail.com' };
-        //         // return user;
-        //         // const { email, password } = credentials;
-        //         // const client = new auth.OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
-        //         // const { tokens } = await client.getToken({ code: password });
-        //         // client.setCredentials(tokens);
-        //         // const { data } = await client.getTokenInfo(tokens.access_token);
-        //         // if (data.email === email) {
-        //         //     return { email: data.email };
-        //         // }
-        //         return user;
-        //     }
-        // })
+        CredentialsPrivider({
+            name: "Sign in",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email",
+                    placeholder: "becario@gmail.com"
+                },
+                password: {
+                    label: "Password",
+                    type: "password"
+                }
+            },
+            async authorize(credentials) {
+                const user = { id: '1', name: 'hola', email: 'bravokevinto@gmail.com' };
+                return user;
+            },
+        }),
     ],
-    secret: process.env.JWT_SECRET!,
-    // callbacks: {
-    //     async jwt(token, user) {
-
-    //         if (user) {
-    //             token.email = user.email;
-    //         }
-    //         return token;
-    //     }
-    // }
-}
+};
 
 export default authOptions; 
