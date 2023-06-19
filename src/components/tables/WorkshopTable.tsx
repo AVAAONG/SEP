c08595d3fdb8030f211e289bccc18634f57947bd
Kevin Bravo <bravokevinto@gmail.com>
2023-06-19 13:36:13 -0400

    feat: add sorting capabilities to the table

--- src/components/tables/WorkshopTable.tsx
+++ src/components/tables/WorkshopTable.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React, { useMemo } from 'react'
 import { Workshop, WorkshopDates, WorkshopSpeaker, WorkshopTempData } from '@prisma/client'
-import { useTable } from 'react-table'
+import { useTable, useSortBy } from 'react-table'
+import { FilterIcon, SortIcon, SortIconReverse } from '@/assets/svgs';
 
 interface WorkshopTableProps {
     workshopData: (Workshop & {
@@ -53,65 +54,32 @@ const WorkshopTable: React.FC<WorkshopTableProps> = (props) => {
         },
 
     ], [])
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data })
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable({ columns, data }, useSortBy)
 
     return (
         <div className="flow-root w-[1300px] overflow-y-scroll h-[600px]">
-            {/* <ul role="list" className='flex flex-col gap-2'>
-                {workshopData.map((workshop) => {
-                    const { speaker, dates, title, id, pensum, spots, avaaYear, platform, modality, tempData } = workshop
-                    return (
-                        <li key={title} className="flex py-2 focus:outline-none focus:outline-offset-0 px-3 rounded-md w-full bg-emerald-950  items-center justify-center gap-4">
-                            <div className="flex-1 w-1/4 ">
-                                <p className="text-sm font-medium truncate text-white">
-                                    {title}
-                                </p>
-                                <p className="text-xs text-gray-500 truncate ">
-                                    Por: {speaker[0].name}
-                                </p>
-                            </div>
-                            <div className="flex-1 text-center w-1/4">
-                                <p className="text-sm font-medium truncate text-white">
-                                    {new Date(dates[0].start_date).getDate()}/{new Date(dates[0].end_date).getMonth()}
-                                </p>
-                                <p className="text-xs text-gray-500 truncate ">
-                                    De {new Date(dates[0].start_date).getUTCHours()} a {new Date(dates[0].end_date).getUTCHours()}
-                                </p>
-                            </div>
-                            <div className="flex-1  text-center w-1/4">
-                                <p className="text-sm font-medium truncate text-white capitalize">
-                                    {pensum.toLocaleLowerCase().replaceAll("_", " ")}
-                                </p>
-                                <p className="text-xs text-gray-500 truncate ">
-                                    {spots} cupos
-                                </p>
-                                <p className="text-xs text-gray-500 truncate ">
-                                    {avaaYear.toString().replaceAll(',', ' y ')} Año
-                                </p>
-                            </div>
-                            <div className="flex-1 min-w-0 text-center w-1/4">
-                                <p className="text-sm font-medium truncate text-white capitalize">
-                                    {modality.toLowerCase()}
-                                </p>
-                                <p className="text-xs text-gray-500 truncate ">
-                                    {platform}
-                                </p>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul> */}
-
-            <table {...getTableProps()} className="w-full text-sm text-left text-gray-300 bg-gradient-to-b from-emerald-950 to-slate-950">
+            <table {...getTableProps()} className="w-full text-sm text-center text-gray-300 bg-gradient-to-b from-emerald-950 to-slate-950">
 
                 <thead className="text-xs text-green-500 uppercase bg-transparent text-center border-b-2 border-green-600 text-ellipsis">
                     {headerGroups.map((headerGroup) => (
                         <tr {...headerGroup.getHeaderGroupProps()} className="text-xs font-medium text-green-500 uppercase tracking-wider">
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()} scope="col" className="px-6 py-3 text-center">
-                                    {column.render('Header')}
-                                </th>
-                            ))}
+                            {headerGroup.headers.map((column) => {
+                                return (
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())} scope="col" className="px-6 py-3">
+                                        <div className='flex text-center gap-2 justify-center items-center'>
+                                            {column.render('Header')}
+                                            {column.isSorted ? column.isSortedDesc ? <SortIconReverse /> : <SortIcon /> : <FilterIcon />}
+                                        </div>
+
+                                    </th>
+                                )
+                            })}
                         </tr>
                     ))}
                 </thead>
