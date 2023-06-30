836dbb84f46a005650e52bc9518c71e60af890b0
Kevin Bravo <bravokevinto@gmail.com>
2023-06-30 17:19:57 -0400

    refactor: separating into compoennts

--- src/components/forms/userSetings/GeneralInformation.tsx
+++ src/components/forms/userSetings/GeneralInformation.tsx
@@ -28,9 +28,9 @@ import React from 'react'
 
 const USER_INPUT_VALUES = [
     {
-        label: 'Nombre Completo',
+        label: 'Nombres',
         id: 'first-name',
-        placeholder: 'Javier Jose',
+        placeholder: 'Kevin José',
         required: true,
         type: 'text',
         name: 'first-name',
@@ -39,24 +39,76 @@ const USER_INPUT_VALUES = [
         tabIndex: 0,
     },
     {
-        label: 'Apellido Completo',
+        label: 'Apellidos',
         id: 'last-name',
-        placeholder: 'Perez Perez',
+        placeholder: 'Bravo Mota',
         required: true,
         type: 'text',
         name: 'last-name',
         autoComplete: 'family-name',
     },
-
     {
-        label: 'Cedula de identidad',
+        label: 'Cédula de identidad',
         id: 'cedula',
         placeholder: 'V-12345678',
         required: true,
         type: 'text',
         name: 'cedula',
         autoComplete: 'cedula',
-    
+    },
+    {
+        label: 'Sexo',
+        id: 'genre',
+        placeholder: 'dd/mm/aaaa',
+        required: true,
+        type: 'select',
+        name: 'genre',
+        autoComplete: 'genre',
+    },
+    {
+        label: 'Fecha de nacimiento',
+        id: 'birthDate',
+        placeholder: 'dd/mm/aaaa',
+        required: true,
+        type: 'date',
+        name: 'birthDate',
+        autoComplete: 'birthDate',
+    },
+    {
+        label: 'Número de telefono local',
+        id: 'localPhone',
+        placeholder: '0212-1234567',
+        required: true,
+        type: 'text',
+        name: 'localPhone',
+        autoComplete: 'localPhone',
+    },
+    {
+        label: 'Número de telefono celular',
+        id: 'cellPhone',
+        placeholder: '0412-1234567',
+        required: true,
+        type: 'text',
+        name: 'cellPhone',
+        autoComplete: 'cellPhone',
+    },
+    {
+        label: 'Correo electronico',
+        id: 'email',
+        placeholder: '',
+        required: true,
+        type: 'email',
+        name: 'email',
+        autoComplete: 'email',
+    },
+    {
+        label: 'Año de ingreso en AVAA',
+        id: 'avaaStartedDate',
+        placeholder: '',
+        required: true,
+        type: 'avaaStartedDate',
+        name: 'avaaStartedDate',
+        autoComplete: 'avaaStartedDate',
     }
 ]
 
@@ -64,57 +116,12 @@ const GeneralInformation = () => {
     return (
         <form action="#">
             <div className="grid grid-cols-6 gap-6">
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="first-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                    <input type="text" name="first-name" id="first-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Bonnie" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="last-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                    <input type="text" name="last-name" id="last-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Green" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="country" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
-                    <input type="text" name="country" id="country" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="United States" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
-                    <input type="text" name="city" id="city" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="e.g. San Francisco" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
-                    <input type="text" name="address" id="address" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="e.g. California" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                    <input type="email" name="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="example@company.com" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="phone-number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                    <input type="number" name="phone-number" id="phone-number" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="e.g. +(12)3456 789" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="birthday" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthday</label>
-                    <input type="number" name="birthday" id="birthday" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="15/08/1990" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="organization" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Organization</label>
-                    <input type="text" name="organization" id="organization" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Company Name" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="role" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                    <input type="text" name="role" id="role" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="React Developer" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="department" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
-                    <input type="text" name="department" id="department" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Development" required />
-                </div>
-                <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="zip-code" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zip/postal code</label>
-                    <input type="number" name="zip-code" id="zip-code" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="123456" required />
-                </div>
-                <div className="col-span-6 sm:col-full">
-                    <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="submit">Guardar</button>
-                </div>
+                {USER_INPUT_VALUES.map((input, index) => (
+                    <div className="col-span-6 sm:col-span-3">
+                        <label htmlFor="first-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{input.label}</label>
+                        <input type="text" name="first-name" id="first-name" placeholder="Bonnie" required />
+                    </div>
+                ))}
             </div>
         </form>
     )
