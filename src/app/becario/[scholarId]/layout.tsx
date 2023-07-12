a459d0b7dabd92ad33f2da7244413f6c7850925b
Kevin Bravo <bravokevinto@gmail.com>
2023-07-12 10:21:43 -0400

    quit unused variables

--- src/app/becario/[scholarId]/layout.tsx
+++ src/app/becario/[scholarId]/layout.tsx
@@ -1,17 +1,6 @@
-import PublicFooter from "@/components/footer/Public"
 import ScholarFooter from "@/components/footer/ScholarFooter"
-import Footer from "@/components/scholar/Footer"
-import Navigation from "@/components/scholar/Navigation"
 import NavigationBar from "@/components/scholar/NavigationBar"
 import Sidebar from "@/components/scholar/Sidebar"
-import TransitionC from "@/components/scholar/Transition"
-import { Children } from "react"
-
-export const metadata = {
-    title: 'Proexcelencia',
-    description: 'Sistema de Evaluacion del Becario',
-    // icons: favicon.src,
-}
 
 export default async function RootLayout({
     children
@@ -26,7 +15,7 @@ export default async function RootLayout({
                 <Sidebar />
                 <main className="p-4 md:ml-64 h-auto pt-16">
                     {children}
-                 <ScholarFooter />
+                    <ScholarFooter />
                 </main>
             </div>
         </>
