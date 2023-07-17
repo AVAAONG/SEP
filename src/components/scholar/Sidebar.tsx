e3cdfa214e07a57490f74d405cee7151c15dd8ab
Kevin Bravo <bravokevinto@gmail.com>
2023-07-17 17:24:48 -0400

    feat: add  highlight button

--- src/components/scholar/Sidebar.tsx
+++ src/components/scholar/Sidebar.tsx
@@ -60,6 +60,11 @@ const Sidebar = () => {
                         <DropdownButton {...item} key={index} />
                     ))}
                 </ul>
+                <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        Registra tus higlights
+                    </span>
+                </button>
                 <div className="p-4 mt-6 rounded-lg bg-emerald-100 dark:bg-emerald-900 dark:border-emerald-950 border-green-600" >
                     <div className="flex items-center mb-3">
                         <span className=" text-emerald-800 text-sm font-bold rounded  dark:text-emerald-200">¡Danos tu feedback!</span>
