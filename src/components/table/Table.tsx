031f7f0bd31079a99fb3015d4e32da09796386be
Kevin Bravo <bravokevinto@gmail.com>
2023-08-12 20:54:22 -0400

    fix: linter error in table component
    fix a linter error in the table component related to the key props
    
    @see https://github.com/TanStack/table/discussions/2647 for more info about the error

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -10,8 +10,8 @@ interface TableProps {
 }
 
 const Table: React.FC<TableProps> = ({ tableData, tableColumns }) => {
-  const data = useMemo(() => tableData, []);
-  const columns = useMemo(() => tableColumns, []);
+  const data = useMemo(() => tableData, [tableData]);
+  const columns = useMemo(() => tableColumns, [tableColumns]);
 
   const {
     getTableProps,
@@ -34,53 +34,61 @@ const Table: React.FC<TableProps> = ({ tableData, tableColumns }) => {
       <div className="flow-root w-full overflow-x-scroll ">
         <table {...getTableProps()} className="w-full text-sm text-left text-gray-300 ">
           <thead className="text-xs text-green-500 uppercase text-center border-b-[1px] border-green-700 text-ellipsis bg-gray-100 dark:bg-slate-950">
-            {headerGroups.map((headerGroup, index) => (
-              <tr
-
-                {...headerGroup.getHeaderGroupProps()}
-                className="text-xs font-medium text-green-700 dark:text-green-500 uppercase tracking-wider"
-              >
-                {headerGroup.headers.map((column, index) => {
-                  return (
-                    <th
-
-                      {...column.getHeaderProps(column.getSortByToggleProps())}
-                      scope="col"
-                      className="px-6 py-3"
-                    >
-                      <div className="flex text-center gap-2 justify-center items-center">
-                        {column.render('Header')}
-                        {column.isSorted ? (
-                          column.isSortedDesc ? (
-                            <SortIconReverse />
+            {headerGroups.map((headerGroup) => {
+              const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps()
+              return (
+                <tr
+                  key={key}
+                  {...restHeaderGroupProps}
+                  className="text-xs font-medium text-green-700 dark:text-green-500 uppercase tracking-wider"
+                >
+                  {headerGroup.headers.map((column) => {
+                    const { getHeaderProps, getSortByToggleProps } = column
+                    const { key, ...restColumn } = getHeaderProps(getSortByToggleProps())
+                    return (
+                      <th
+                        key={key}
+                        {...restColumn}
+                        scope="col"
+                        className="px-6 py-3"
+                      >
+                        <div className="flex text-center gap-2 justify-center items-center">
+                          {column.render('Header')}
+                          {column.isSorted ? (
+                            column.isSortedDesc ? (
+                              <SortIconReverse />
+                            ) : (
+                              <SortIcon />
+                            )
                           ) : (
-                            <SortIcon />
-                          )
-                        ) : (
-                          <FilterIcon />
-                        )}
-                      </div>
-                    </th>
-                  );
-                })}
-              </tr>
-            ))}
+                            <FilterIcon />
+                          )}
+                        </div>
+                      </th>
+                    );
+                  })}
+                </tr>
+              )
+
+            })}
           </thead>
 
           <tbody {...getTableBodyProps()} className="divide-y divide-gray-500 dark:divide-gray-700">
-            {page.map((row, index) => {
+            {page.map((row) => {
               prepareRow(row);
+              const { key, ...restRowProps } = row.getRowProps();
               return (
                 <tr
-
-                  {...row.getRowProps()}
+                  key={key}
+                  {...restRowProps}
                   className="text-sm hover:bg-green-500 dark:hover:bg-green-700 dark:hover:text-white text-center text-gray-800 dark:text-gray-300"
                 >
-                  {row.cells.map((cell, index) => {
+                  {row.cells.map((cell) => {
+                    const { key, ...restCellProps } = cell.getCellProps();
                     return (
                       <td
-
-                        {...cell.getCellProps()}
+                        key={key}
+                        {...restCellProps}
                         className="px-4 py-2 whitespace-nowrap lowecase capitalize"
                       >
                         {cell.render('Cell')}
