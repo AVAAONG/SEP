d25572d36fff631e61c02d27e422ee4ca6fb5a22
Kevin Bravo <bravokevinto@gmail.com>
2023-08-30 22:44:12 -0400

    feat: allow to delete the dates

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,50 +1,60 @@
 import React, { useState } from 'react';
 import { UseFormRegister } from 'react-hook-form';
-
 type Props = {
   register: UseFormRegister<any>;
 };
 
-const ActivityForm: React.FC<Props> = ({ register }) => {
-  const [count, setCount] = useState(1);
+const DateInput: React.FC<Props> = ({ register }) => {
+  const [inputs, setInputs] = useState([{ id: 0, date: '', startHour: '', endHour: '' }]);
 
   const addInput = () => {
-    setCount(count + 1);
+    const newId = inputs.length;
+    setInputs([...inputs, { id: newId, date: '', startHour: '', endHour: '' }]);
+  };
+
+  const deleteInput = (id: number) => {
+    const newInputs = inputs.filter((input) => input.id !== id);
+    setInputs(newInputs);
   };
 
   return (
     <>
-      {[...Array(count)].map((_, i) => (
-        <>
-          <div key={i} className="col-span-2 md:col-span-1">
-            <button
-              type="button"
-              onClick={addInput}
-              className="inline h-6 w-6 bg-green-700 rounded-full text-base font-bold text-white absolute translate-x-64 translate-y-2"
-            >
-              +
-            </button>
-            <label className="block mb-2 text-xs m-l-1 font-semibold text-slate-400 uppercase ">
-              Fecha
-            </label>
-            <input type="date" {...register(`date.${i}`)} />
-          </div>
-          <div key={i} className="col-span-2 md:col-span-1">
-            <label className="block mb-2 text-xs m-l-1 font-semibold text-slate-400 uppercase ">
-              Hora de inicio
-            </label>
-            <input type="time" {...register(`startHour.${i}`)} />
-          </div>
-          <div key={i} className="col-span-2 md:col-span-1">
-            <label className="block mb-2 text-xs m-l-1 font-semibold text-slate-400 uppercase ">
-              Hora de cierre
-            </label>
-            <input type="time" {...register(`endHour.${i}`)} />
-          </div>
-        </>
-      ))}
+      {inputs.map((input, index) => {
+        const { id, date, startHour, endHour } = input;
+        const isLastInput = index === inputs.length - 1;
+        return (
+          <>
+            < div className="col-span-2 md:col-span-1 relative" >
+              <button
+                type="button"
+                onClick={isLastInput ? addInput : () => deleteInput(id)}
+                className={`inline h-6 w-6 rounded-full text-base font-bold text-white absolute ${isLastInput ? 'bg-green-700 translate-x-64 translate-y-2' : 'bg-red-700 translate-x-56 translate-y-2'
+                  }`}
+              >
+                {isLastInput ? '+' : '-'}
+              </button>
+              <label className="block mb-2 text-xs m-l-1 font-semibold text-slate-400 uppercase ">
+                Fecha {index + 1}
+              </label>
+              <input type="date" {...register(`date.${id}` as const)} defaultValue={date} />
+            </div >
+            <div className="col-span-2 md:col-span-1">
+              <label className="block mb-2 text-xs m-l-1 font-semibold text-slate-400 uppercase ">
+                Hora de inicio {index + 1}
+              </label>
+              <input type="time" {...register(`startHour.${id}` as const)} defaultValue={startHour} />
+            </div>
+            <div className="col-span-2 md:col-span-1">
+              <label className="block mb-2 text-xs m-l-1 font-semibold text-slate-400 uppercase ">
+                Hora de cierre {index + 1}
+              </label>
+              <input type="time" {...register(`endHour.${id}` as const)} defaultValue={endHour} />
+            </div>
+          </>
+        );
+      })}
     </>
   );
 };
 
-export default ActivityForm;
+export default DateInput;
