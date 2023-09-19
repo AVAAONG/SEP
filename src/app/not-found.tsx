fe2cf1ac9ee6c8525e55180eb1e03759c982bfe9
Kevin Bravo <bravokevinto@gmail.com>
2023-09-19 16:50:47 -0400

    feat: access denied page and refactor not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -17,37 +17,35 @@ import { NotFoundImage } from '../../public/svgs/svgs';
  */
 const NotFound = () => {
   return (
-    <>
-      <div className="bg-light dark:bg-dark min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-        <div className="max-w-max mx-auto">
-          <div className="max-w-xs m-auto">
-            <NotFoundImage />
-          </div>
-          <main className="sm:flex">
-            <p className="text-4xl font-extrabold text-primary-light sm:text-5xl">404</p>
-            <div className="sm:ml-6">
-              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-                <h1 className="text-4xl font-extrabold text-gray-900 dark:text-gray-200 tracking-tight sm:text-5xl">
-                  Página no encontrada.
-                </h1>
-                <p className="mt-1 text-base text-gray-500">
-                  Verifique la URL en la barra de direcciones y vuelva a intentarlo.
-                </p>
-              </div>
-              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                <BackButton />
-                <Link
-                  href="mailto:avaatecnologia@gmail.com"
-                  className="inline-flex items-center px-4 py-2 border-primary-light border border-transparent text-sm font-medium rounded-md text-primary-light bg-secondary-1 hover:bg-secondary-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-light"
-                >
-                  Contacta con soporte
-                </Link>
-              </div>
-            </div>
-          </main>
+    <div className="bg-light dark:bg-dark min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="max-w-max mx-auto">
+        <div className="max-w-xs m-auto">
+          <NotFoundImage />
         </div>
+        <main className="sm:flex">
+          <p className="text-4xl font-extrabold text-primary-light sm:text-5xl">404</p>
+          <div className="sm:ml-6">
+            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 className="text-4xl font-extrabold text-gray-900 dark:text-gray-200 tracking-tight sm:text-5xl">
+                Página no encontrada. <span className="font-normal">🤔</span>
+              </h1>
+              <p className="mt-1 text-base text-gray-500">
+                Verifique la URL en la barra de direcciones y vuelva a intentarlo.
+              </p>
+            </div>
+            <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+              <BackButton />
+              <Link
+                href="mailto:avaatecnologia@gmail.com"
+                className="inline-flex items-center px-4 py-2 border-primary-light border border-transparent text-sm font-medium rounded-md text-primary-light bg-secondary-1 hover:bg-secondary-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-light"
+              >
+                Contacta con soporte
+              </Link>
+            </div>
+          </div>
+        </main>
       </div>
-    </>
+    </div>
   );
 };
 
