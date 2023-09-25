bdce97a0c6a8b718c2f6d9ca4c5442459acf19f6
Kevin Bravo <bravokevinto@gmail.com>
2023-09-25 02:05:33 -0400

    fix: support new gender type

--- src/lib/storeInDatabase.ts
+++ src/lib/storeInDatabase.ts
@@ -118,56 +118,56 @@
 // }
 
 //====================================================== Speaker Functions ======================================================
-const WORKSHOP_SPEAKERS_SPREADSHEET = '1uGrF-GNSILOXVOy3SLZUvx47HXzcJMv4TaJHJ26aBLc';
-const WORKSHOP_SPEAKER_SHEET = 'Facilitadores de talleres';
-const WORKSHOP_SPEAKERS_RANGE = `'${WORKSHOP_SPEAKER_SHEET}'!B2:I90`;
+// const WORKSHOP_SPEAKERS_SPREADSHEET = '1uGrF-GNSILOXVOy3SLZUvx47HXzcJMv4TaJHJ26aBLc';
+// const WORKSHOP_SPEAKER_SHEET = 'Facilitadores de talleres';
+// const WORKSHOP_SPEAKERS_RANGE = `'${WORKSHOP_SPEAKER_SHEET}'!B91:I91`;
 
 // const createWorkshopSpeakerFromSpreadsheet = async () => {
-//     const values = (await getSpreadsheetValues(
-//         WORKSHOP_SPEAKERS_SPREADSHEET,
-//         WORKSHOP_SPEAKERS_RANGE
-//     )) as string[][];
+//   const values = (await getSpreadsheetValues(
+//     WORKSHOP_SPEAKERS_SPREADSHEET,
+//     WORKSHOP_SPEAKERS_RANGE
+//   )) as string[][];
 
-//     const workshopSpeakers = values.map((value) => {
-//         const [
-//             first_names,
-//             last_names1,
-//             last_names2,
-//             id,
-//             email,
-//             phone_number,
-//             job_company,
-//             speakerGender,
-//         ] = value;
-//         const gender = speakerGender.toLowerCase() === 'masculino' ? 'M' : 'F';
-//         const workshopSpeaker: WorkshopSpeaker = {
-//             id,
-//             first_names,
-//             last_names: `${last_names1} ${last_names2}`,
-//             email: email ? email.toLowerCase() : null,
-//             birthdate: null,
-//             years_of_exp: null,
-//             job_title: null,
-//             job_company: job_company ? job_company.toLowerCase() : null,
-//             actual_city: null,
-//             gender,
-//             actual_country: null,
-//             curriculum: null,
-//             description: null,
-//             facebook_user: null,
-//             image: null,
-//             instagram_user: null,
-//             linkedin_user: null,
-//             phone_number: phone_number ? phone_number.replace(/\s/g, '') : null,
-//             twitter_user: null,
-//         };
-//         // await createWorkshopSpeaker(workshopSpeaker);
-//         return workshopSpeaker;
-//     });
-
-//     return workshopSpeakers;
+//   const workshopSpeakers = values.map(async (value) => {
+//     const [
+//       first_names,
+//       last_names1,
+//       last_names2,
+//       id,
+//       email,
+//       phone_number,
+//       job_company,
+//       speakerGender,
+//     ] = value;
+//     let gender;
+//     if (speakerGender === "O") gender = "O"
+//     else gender = speakerGender.toLowerCase() === 'masculino' ? 'M' : 'F';
+//     const workshopSpeaker: WorkshopSpeaker = {
+//       id,
+//       first_names,
+//       last_names: `${last_names1} ${last_names2}`,
+//       email: email ? email.toLowerCase() : null,
+//       birthdate: null,
+//       years_of_exp: null,
+//       job_title: null,
+//       job_company: job_company ? job_company.toLowerCase() : null,
+//       actual_city: null,
+//       gender,
+//       actual_country: null,
+//       curriculum: null,
+//       description: null,
+//       facebook_user: null,
+//       image: null,
+//       instagram_user: null,
+//       linkedin_user: null,
+//       phone_number: phone_number ? phone_number.replace(/\s/g, '') : null,
+//       twitter_user: null,
+//     };
+//     await createWorkshopSpeaker(workshopSpeaker);
+//     return workshopSpeaker;
+//   });
+//   return workshopSpeakers;
 // };
-
 
 ////// functions to create workshops
 
