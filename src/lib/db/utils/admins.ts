3cae9a78b79fa779846023faf72c54077dba3b72
Kevin Bravo <bravokevinto@gmail.com>
2023-10-02 13:40:33 -0400

    feat: add function to create admins

--- src/lib/db/utils/admins.ts
+++ src/lib/db/utils/admins.ts
@@ -11,3 +11,8 @@ export const createAdminUser = async (data: AdminUser): Promise<AdminUser> => {
     const user = await prisma.adminUser.create({ data });
     return user;
 }
+
+export const deleteAdmin = async (adminId: string) => {
+    const user = await prisma.adminUser.delete({ where: { id: adminId } });
+    return user;
+}
