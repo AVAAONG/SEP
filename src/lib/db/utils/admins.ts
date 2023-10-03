3f06687fae31969c1fd6aef3233742f1bcacf279
Kevin Bravo <bravokevinto@gmail.com>
2023-10-03 18:04:29 -0400

    feat: add function to retreive only one admin and to delete admin

--- src/lib/db/utils/admins.ts
+++ src/lib/db/utils/admins.ts
@@ -1,17 +1,23 @@
+'use server'
+
 import { AdminProfile } from "@prisma/client";
 import { prisma } from "./prisma";
-
 
 export const getAdminsProfiles = async () => {
     const users = await prisma.adminProfile.findMany();
     return users;
 };
 
-
+export const getAdmin = async (email: string) => {
+    const admin = await prisma.adminProfile.findUnique({
+        where: { allowedEmail: email }
+    });
+    return admin;
+}
 
 //durante la creacion, en el profile buscamos el usuario y lo retornamos
 export const createAdminProfileUser = async (data: AdminProfile) => {
-    await prisma.adminProfile.create({
+    const adminProfile = await prisma.adminProfile.create({
         data: {
             allowedEmail: data.allowedEmail,
             gender: data.gender,
@@ -21,6 +27,7 @@ export const createAdminProfileUser = async (data: AdminProfile) => {
             profileImage: data.profileImage,
         },
     });
+    return adminProfile;
 };
 
 export const updateAdminProfileUser = async (data: AdminProfile) => {
@@ -28,6 +35,15 @@ export const updateAdminProfileUser = async (data: AdminProfile) => {
 }
 
 export const deleteAdmin = async (adminId: string) => {
-    const user = await prisma.adminUser.delete({ where: { id: adminId } });
+    const user = await prisma.adminProfile.delete({
+        where: { id: adminId }, include: {
+            adminUser: {
+                include: {
+                    accounts: true,
+                    sessions: true
+                }
+            }
+        }
+    });
     return user;
 }
