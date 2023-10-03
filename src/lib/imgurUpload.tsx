6e8cf72488bde6a5a455514a360a40474abec354
Kevin Bravo <bravokevinto@gmail.com>
2023-10-03 18:05:12 -0400

    feat: function to upload to imgur

--- src/lib/imgurUpload.tsx
+++ src/lib/imgurUpload.tsx
@@ -1,22 +1,39 @@
-import { ImgurClient } from 'imgur';
+'use client';
+import { Buffer } from 'buffer';
 
-const client = new ImgurClient({
-  clientId: '<YOUR_CLIENT_ID>',
-});
+export const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID!;
 
-async function uploadImage(image: File): Promise<string> {
-  const response = await client.upload({
-    image,
-    type: 'stream',
+/**
+ * Uploads an image to Imgur.
+ *
+ * @param image The image to upload.
+ * @returns The Imgur link to the uploaded image.
+ * @example
+```typescript
+const image = new File(["image data"], "image.png");
+const imgurLink = await uploadToImgur(image);
+```
+ */
+export const uploadImageToImgur = async (image: File): Promise<string> => {
+  console.log(IMGUR_CLIENT_ID);
+  const imageBuffer = await image.arrayBuffer();
+  const base64Image = Buffer.from(imageBuffer).toString('base64');
+
+  const response = await fetch('https://api.imgur.com/3/image', {
+    method: 'POST',
+    headers: {
+      Authorization: `Client-ID 3e335264db5d41e`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image: base64Image,
+    }),
   });
 
-  return response.data.link;
-}
+  if (response.status !== 200) {
+    throw new Error(`Failed to upload image to Imgur: ${response.status}`);
+  }
 
-// Example usage:
-
-const imageFile = new File(['<IMAGE_CONTENT>'], 'image.jpg');
-
-const imageUrl = await uploadImage(imageFile);
-
-console.log(imageUrl); // Output: https://i.imgur.com/1234567890.jpg
+  const data = await response.json();
+  return data.data.link;
+};
