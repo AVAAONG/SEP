35b18772c563caeb68c13e5cf1e7ccabe22095f1
Kevin Bravo <bravokevinto@gmail.com>
2023-11-13 13:24:29 -0400

    feat: add function to share public profile (create qrcode)

---
+++ src/components/ShareModal.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { ClipboardIcon, ShareIcon } from '@heroicons/react/24/outline';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from '@nextui-org/react';
+import Image from 'next/image';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+interface ShareModalProps {
+  qrCode: string;
+  profileLink: string;
+}
+
+const SharePubilcProfile: React.FC<ShareModalProps> = ({ qrCode, profileLink }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button radius="lg" variant="ghost" onPress={onOpen} isIconOnly className="p-2">
+        <ShareIcon />
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="w-full text-center">Compartir perfil</ModalHeader>
+              <ModalBody>
+                <div className="flex flex-col items-center gap-6 w-full">
+                  <div className="w-full flex rounded-full items-center justify-between gap-2 p-1 bg-gray-100">
+                    <p className="font-semibold truncate ml-4">{profileLink}</p>
+                    <Button radius="full" isIconOnly className="p-2">
+                      <ClipboardIcon />
+                    </Button>
+                  </div>
+                  <p className="text-sm text-center">
+                    Escanea el codigo QR o haz clic sobre la imagen para copiarla
+                  </p>
+                  <CopyToClipboard text={qrCode}>
+                    <Button className="h-full py-4">
+                      <Image
+                        className="cursor-pointer"
+                        src={qrCode}
+                        alt="Codigo QR"
+                        width={350}
+                        height={300}
+                      />
+                    </Button>
+                  </CopyToClipboard>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Cerrar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default SharePubilcProfile;
---
+++ src/lib/utils/createQrCode.ts
@@ -0,0 +1,29 @@
+// Install the library first: npm install --save qrcode
+
+import * as QRCode from 'qrcode';
+
+/**
+ * Generates a QR code from the input string.
+ * @param inputText The text to encode into the QR code.
+ * @param outputPath Optional path to save the QR code image (e.g., 'output.png').
+ * @param errorCorrectionLevel Optional error correction level ('L', 'M', 'Q', or 'H').
+ * @param width Optional image width in pixels.
+ * @param scale Optional scale factor.
+ */
+async function generateQRCode(
+    inputText: string,
+) {
+    try {
+        const qrDataURL = await QRCode.toDataURL(inputText, {
+            margin: 1,
+            width: 300,
+            errorCorrectionLevel: 'H',
+        });
+        return qrDataURL;
+
+    } catch (error) {
+        console.error('Error generating QR code:', error);
+    }
+}
+
+export default generateQRCode;
