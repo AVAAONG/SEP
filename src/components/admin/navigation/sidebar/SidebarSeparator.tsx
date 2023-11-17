3e77ca2eea40b17965c527f2011d5ab2d79cb56b
Kevin Bravo <bravokevinto@gmail.com>
2023-11-17 15:14:57 -0400

    style: change button style in sidebar, close dropdown when page change in mob

--- src/components/admin/navigation/sidebar/SidebarSeparator.tsx
+++ src/components/admin/navigation/sidebar/SidebarSeparator.tsx
@@ -1,7 +1,7 @@
 const SidebarSeparator = ({ text }: { text: string }) => {
   return (
-    <div className="border-t border-gray-300 dark:border-emerald-800 p-1 font-medium w-full ">
-      <p className="text-xs font-bold text-black dark:text-primary-light mt-2 tracking-wider ">
+    <div className="border-t border-gray-300 dark:border-emerald-800 p-1 mt-2 md:mt-0 font-medium w-full ">
+      <p className="text-xs font-medium text-black dark:text-primary-light mt-2 tracking-wider ">
         {text}
       </p>
     </div>
--- src/components/scholar/DropdownButton.tsx
+++ src/components/scholar/DropdownButton.tsx
@@ -1,6 +1,8 @@
 'use client';
+import { Button } from '@nextui-org/react';
 import Link from 'next/link';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { ChevronIcon } from '../../../public/svgs/svgs';
 
 type DropdownButtonProps = {
@@ -12,16 +14,23 @@ type DropdownButtonProps = {
 
 const DropdownButton = (props: DropdownButtonProps) => {
   const { buttonName, itemList, Icon, link } = props;
+  const pathname = usePathname();
 
   const [isDropdownOpen, setDropdown] = useState(false);
   const toggleDropdown = () => setDropdown(!isDropdownOpen);
+  useEffect(() => {
+    // Function to handle route changes
+    if (window.innerWidth <= 768) {
+      setDropdown(false);
+    }
+  }, [pathname]);
 
   if (itemList.length === 0) {
     return (
       <ul>
         <Link
           href={link ? link : ''}
-          className="flex items-center p-2 text-sm font-medium rounded-lg text-gray-200 hover:text-white"
+          className="flex items-center p-2 text-sm font-medium text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-white"
         >
           <div className="w-6 h-6">{Icon}</div>
           <span className="ml-3">{buttonName}</span>
@@ -31,17 +40,19 @@ const DropdownButton = (props: DropdownButtonProps) => {
   } else {
     return (
       <ul>
-        <button
+        <Button
           type="button"
-          className="flex items-center p-2 w-full text-sm font-medium rounded-lg text-gray-200 hover:text-white"
+          className="text-white dark:text-gray-300 font-medium w-full "
+          radius="md"
+          variant="light"
           onClick={toggleDropdown}
         >
           <div className="w-6 h-6">{Icon}</div>
-          <span className="flex-1 ml-3 text-left whitespace-nowrap">{buttonName}</span>
-          <div className={`${isDropdownOpen ? 'rotate-180' : ''} w-6 h-6`}>
+          <span className="flex-1 ml-2 text-left whitespace-nowrap">{buttonName}</span>
+          <div className={`${isDropdownOpen ? 'rotate-180 transition-transform  ' : ''} w-6 h-6`}>
             <ChevronIcon />
           </div>
-        </button>
+        </Button>
         <ul
           id="dropdown-pages"
           className={`${
@@ -52,7 +63,7 @@ const DropdownButton = (props: DropdownButtonProps) => {
             <li className="flex gap-2 justify-center items-center" key={index}>
               <Link
                 href={item.link}
-                className="flex items-center gap-4 p-2 pl-11 w-full text-sm rounded-lg text-gray-200 hover:text-white"
+                className="flex items-center gap-4 p-2 pl-11 w-full text-sm rounded-lg text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-white"
               >
                 <div className="rounded-full bg-gray-200 hover:bg-white w-1.5 h-1.5"></div>
                 {item.name}
