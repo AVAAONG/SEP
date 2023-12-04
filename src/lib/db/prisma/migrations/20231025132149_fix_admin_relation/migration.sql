eee5017e5584ca9e3173e793dce4d1842bd94e4e
Kevin Bravo <bravokevinto@gmail.com>
2023-12-04 17:02:44 -0400

    build: delete prisma migrations history and start again

--- src/lib/db/prisma/migrations/20231025132149_fix_admin_relation/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "AdminProfile_allowedActions_id_key";
-
--- DropIndex
-DROP INDEX "AdminProfile_chapter_id_idx";
-
--- CreateIndex
-CREATE INDEX "AdminProfile_chapter_id_allowedActions_id_idx" ON "AdminProfile"("chapter_id", "allowedActions_id");
--- src/lib/db/prisma/migrations/20231122175219_add_regime/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "StudyRegime" ADD VALUE 'QUARTIER';
