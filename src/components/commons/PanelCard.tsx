698a66fa4844400da877c34fe1d02a3c845881b0
Kevin Bravo <bravokevinto@gmail.com>
2024-01-09 12:01:06 -0400

    style: background colors and dark mode

--- src/components/commons/PanelCard.tsx
+++ src/components/commons/PanelCard.tsx
@@ -5,25 +5,25 @@ const colorVariants = {
     iconColor: 'bg-gradient-to-br from-blue-700  to-indigo-900',
     titleColor: 'text-blue-700',
     subtitleColor: 'text-blue-600 hover:text-blue-500',
-    linkBackground: 'bg-blue-50',
+    linkBackground: 'bg-blue-50  dark:bg-blue-950  hover:bg-blue-100 dark:hover:bg-blue-900',
   },
   chat: {
     iconColor: 'bg-gradient-to-r from-red-500 to-red-900',
     titleColor: 'text-red-500',
     subtitleColor: 'text-red-600 hover:text-red-500',
-    linkBackground: 'bg-red-50',
+    linkBackground: 'bg-red-50  dark:bg-red-950 hover:bg-red-100 dark:hover:bg-red-900',
   },
   volunteer: {
     iconColor: 'bg-gradient-to-r from-green-500 to-green-900',
     titleColor: 'text-green-500',
     subtitleColor: 'text-green-600 hover:text-green-500',
-    linkBackground: 'bg-green-50',
+    linkBackground: 'bg-green-50 dark:bg-green-950 hover:bg-green-100 dark:hover:bg-green-900',
   },
   scholar: {
     iconColor: 'bg-gradient-to-br from-yellow-500 to-yellow-700',
     titleColor: 'text-yellow-500',
     subtitleColor: 'text-yellow-600 hover:text-yellow-500',
-    linkBackground: 'bg-yellow-50',
+    linkBackground: 'bg-yellow-50 dark:bg-yellow-950 hover:bg-yellow-100 dark:hover:bg-yellow-900',
   },
 };
 
@@ -38,7 +38,7 @@ export interface PanelCardProps {
 
 const PanelCard: React.FC<PanelCardProps> = ({ title, subtitle, data, link, icon, kind }) => {
   return (
-    <div className="w-full relative border-1 border-gray-300 bg-white dark:bg-black pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
+    <div className="w-full relative border-1 border-gray-300 dark:border-secondary-dark bg-white dark:bg-black pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
       <dt>
         <div className={`absolute ${colorVariants[kind]['iconColor']} rounded-md p-3`}>
           <div className="w-6 h-6 text-white">{icon}</div>
@@ -50,12 +50,12 @@ const PanelCard: React.FC<PanelCardProps> = ({ title, subtitle, data, link, icon
       <dd className="ml-16 pb-3 flex items-baseline">
         <p className="text-3xl font-semibold ">{data}</p>
         <div
-          className={`absolute bottom-0 inset-x-0 ${colorVariants[kind]['linkBackground']} dark:bg-black px-4 py-3`}
+          className={`absolute bottom-0 inset-x-0 ${colorVariants[kind]['linkBackground']} dark:bg-black px-4 py-3 `}
         >
           <Link
             color="foreground"
             href={link}
-            className={`font-medium text-sm  ${colorVariants[kind]['subtitleColor']}`}
+            className={`block font-medium text-sm  ${colorVariants[kind]['subtitleColor']} w-full text-left truncate`}
           >
             {subtitle}
           </Link>
--- src/components/scholar/Sidebar.tsx
+++ src/components/scholar/Sidebar.tsx
@@ -80,7 +80,7 @@ const Sidebar = () => {
     <aside
       className={`${
         isSidebarOpen ? 'fixed  w-full md:w-72' : 'hidden'
-      } top-0 left-0 z-40 h-screen pt-4   md:translate-x-0 dark:bg-black`}
+      } top-0 left-0 z-40 h-screen pt-4 bg-gray-50 md:translate-x-0 dark:bg-black`}
     >
       <div className={`flex items-center mt-2 px-5 ${isSidebarOpen ? 'justify-between' : ''} `}>
         <Link href="/becario/dashboard" className="flex justify-center w-full">
