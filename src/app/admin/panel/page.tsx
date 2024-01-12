abee66f2d5e6784e83a3ec35cd86f7ef8535c1f0
Kevin Bravo <bravokevinto@gmail.com>
2024-01-12 10:53:03 -0400

    refactor: replace hardcoded cards in admin panel

--- src/app/admin/panel/page.tsx
+++ src/app/admin/panel/page.tsx
@@ -1,9 +1,9 @@
 import NextEventsList from '@/components/NextEventsList';
 import Calendar from '@/components/calendar/Calendar';
+import PanelCard, { PanelCardProps } from '@/components/commons/PanelCard';
 import { getActivitiesByYear } from '@/lib/db/utils/Workshops';
 import { getScholarsCountByCondition } from '@/lib/db/utils/users';
 import { formatActivityEventsForBigCalendar } from '@/lib/utils';
-import { Link } from '@nextui-org/react';
 import { Chat, Workshop } from '@prisma/client';
 import { chatIcon, userIcon, workshopIcon } from 'public/svgs/svgs';
 
@@ -17,76 +17,52 @@ const page = async () => {
       (activity) => activity.activity_status === 'SENT' || activity.activity_status === 'SCHEDULED'
     )
     .sort((a, b) => new Date(a.start_dates[0]).getTime() - new Date(b.start_dates[0]).getTime());
-  const workshopDoneCount = workshops.filter((workshop) => workshop.activity_status === 'DONE' || workshop.activity_status === 'ATTENDANCE_CHECKED').length;
-  const chatsDoneCount = chats.filter((chat) => chat.activity_status === 'DONE' || chat.activity_status === 'ATTENDANCE_CHECKED').length;
+  const workshopDoneCount = workshops.filter(
+    (workshop) =>
+      workshop.activity_status === 'DONE' || workshop.activity_status === 'ATTENDANCE_CHECKED'
+  ).length;
+  const chatsDoneCount = chats.filter(
+    (chat) => chat.activity_status === 'DONE' || chat.activity_status === 'ATTENDANCE_CHECKED'
+  ).length;
+
+  const cardContent: PanelCardProps[] = [
+    {
+      title: 'Actividades formativas realizadas',
+      subtitle: 'Ver todas las actividades',
+      data: workshopDoneCount,
+      link: 'actividadesFormativas',
+      icon: workshopIcon(),
+      kind: 'workshop',
+    },
+    {
+      title: 'Chat clubs de inglés realizados',
+      subtitle: 'Ver todos los chats',
+      data: chatsDoneCount,
+      link: 'chats',
+      icon: chatIcon(),
+      kind: 'chat',
+    },
+    {
+      title: 'Horas de voluntariado realizadas',
+      subtitle: 'Ver todas las actividades',
+      data: 0,
+      link: 'actividadesFormativas',
+      icon: workshopIcon(),
+      kind: 'volunteer',
+    },
+    {
+      title: 'Total de becarios activos',
+      subtitle: 'Ver base de datos de becarios',
+      data: activeScholarsCount,
+      link: 'becarios',
+      icon: userIcon(),
+      kind: 'scholar',
+    },
+  ];
   return (
     <div className="flex flex-col gap-4 h-full w-full">
       <div className="w-full flex flex-col md:flex-row gap-3 items-center">
-        <div className="w-full md:w-1/4 relative bg-white dark:bg-black pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
-          <dt>
-            <div className="absolute bg-gradient-to-br from-blue-700  to-indigo-900 rounded-md p-3">
-              <div className="w-6 h-6 text-white">{workshopIcon()}</div>
-            </div>
-            <p className="ml-16 font-medium text-blue-700  truncate">
-              Actividades formativas realizadas
-            </p>
-          </dt>
-          <dd className="ml-16 pb-3 flex items-baseline ">
-            <p className="text-3xl font-semibold">{workshopDoneCount}</p>
-
-            <div className="absolute bottom-0 inset-x-0 bg-blue-50 dark:bg-black px-4 py-3">
-              <Link
-                href="actividadesFormativas/lista"
-                className="font-medium text-sm text-blue-600 hover:text-blue-500"
-              >
-                Ver todas las actividades
-              </Link>
-            </div>
-          </dd>
-        </div>
-        <div className="w-full md:w-1/4 relative bg-white dark:bg-black pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
-          <dt>
-            <div className="absolute bg-gradient-to-r from-red-500  to-red-900 rounded-md p-3">
-              <div className="w-6 h-6 text-white">{chatIcon()}</div>
-            </div>
-            <p className="ml-16 font-medium text-red-500 truncate">
-              Chat clubs de inglés realizados
-            </p>
-          </dt>
-          <dd className="ml-16 pb-3 flex items-baseline">
-            <p className="text-3xl font-semibold ">{chatsDoneCount}</p>
-
-            <div className="absolute bottom-0 inset-x-0 bg-red-50 dark:bg-black px-4 py-3">
-              <Link
-                color="foreground"
-                href="chats/lista"
-                className="font-medium text-sm text-red-600 hover:text-red-500"
-              >
-                Ver todos los chats
-              </Link>
-            </div>
-          </dd>
-        </div>
-        <div className="w-full md:w-1/4  relative bg-white dark:bg-black pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
-          <dt>
-            <div className="absolute bg-gradient-to-br from-yellow-500 to-yellow-700 rounded-md p-3">
-              <div className="w-6 h-6 text-white">{userIcon()}</div>
-            </div>
-            <p className="ml-16 font-medium text-yellow-500 truncate">Total de becarios activos</p>
-          </dt>
-          <dd className="ml-16 pb-3  flex items-baseline  ">
-            <p className="text-3xl font-semibold ">{activeScholarsCount}</p>
-
-            <div className="absolute bottom-0 inset-x-0 bg-yellow-50 dark:bg-black px-4 py-3">
-              <Link
-                href="becarios"
-                className="font-medium text-sm text-yellow-600 hover:text-yellow-500"
-              >
-                Ver base de datos de becarios
-              </Link>
-            </div>
-          </dd>
-        </div>
+        {cardContent.map((card) => PanelCard(card))}
       </div>
       <div className="flex flex-col lg:flex-row gap-2 ">
         <div className="h-full max-h-[680px] w-full overflow-x-clip rounded-md backdrop-filter backdrop-blur-3xl bg-white dark:bg-black shadow-md p-2">
