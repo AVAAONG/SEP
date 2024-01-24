3c49915a2e6c1a6024474aae9a6b283fb54bd379
Kevin Bravo <bravokevinto@gmail.com>
2024-01-24 17:15:55 -0400

    refactor: add key in map

--- src/components/admin/navigation/sidebar/Sidebar.tsx
+++ src/components/admin/navigation/sidebar/Sidebar.tsx
@@ -23,8 +23,9 @@ const Sidebar = () => {
   }, [pathname]);
   return (
     <aside
-      className={`${isOpen ? 'fixed xl:flex xl:relative' : 'hidden'
-        } flex-col gap-4 justify-start items-center z-50 xl:z-0 w-full sm:w-72 py-8 px-2 bg-primary-light dark:bg-secondary-dark transition-all min-h-full`}
+      className={`${
+        isOpen ? 'fixed xl:flex xl:relative' : 'hidden'
+      } flex-col gap-4 justify-start items-center z-50 xl:z-0 w-full sm:w-72 py-8 px-2 bg-primary-light dark:bg-secondary-dark transition-all min-h-full`}
     >
       <div className="flex justify-between xl:justify-start items-center mb-8 xl:mb-2">
         <Link href="/admin/panel">
@@ -69,6 +70,7 @@ const Sidebar = () => {
         {SCHOLARS.map(({ buttonName, icon, itemList, link }) => {
           return (
             <DropdownButton
+              key={buttonName}
               buttonName={buttonName}
               Icon={icon()}
               itemList={itemList}
@@ -113,7 +115,7 @@ const Sidebar = () => {
           link="/postulantes/registro"
         />
       </div>
-    </aside >
+    </aside>
   );
 };
 
