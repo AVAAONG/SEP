3e43857068b7bff687dad1397d15e4f11cf05f26
Kevin Bravo <bravokevinto@gmail.com>
2024-01-24 05:22:16 -0400

    feat: allow set multiple dates when creating an activity

--- src/components/commons/DateInput.tsx
+++ src/components/commons/DateInput.tsx
@@ -1,130 +1,143 @@
+import workshopCreationFormSchema from '@/lib/schemas/workshopCreationFormSchema';
 import { Input } from '@nextui-org/input';
-import { nanoid } from 'nanoid';
-import React, { useState } from 'react';
-import { Control, Controller } from 'react-hook-form';
+import { Button } from '@nextui-org/react';
+import React, { useEffect } from 'react';
+import { Control, Controller, UseFieldArrayReturn } from 'react-hook-form';
+import { z } from 'zod';
 
-type Props = {
-  control: Control<any, any>;
+/**
+ * Props for the DateInput component
+ * @typedef {Object} Props
+ * @property {Control<z.infer<typeof workshopCreationFormSchema>>} control - A Control object from react-hook-form that is used to control the form state.
+ * @property {UseFieldArrayReturn<z.infer<typeof workshopCreationFormSchema>>} fieldArray - A UseFieldArrayReturn object from react-hook-form that is used to manage a list of fields.
+ */
+interface DateInputProps {
+  control: Control<z.infer<typeof workshopCreationFormSchema>>;
+  fieldArray: UseFieldArrayReturn<z.infer<typeof workshopCreationFormSchema>>;
 };
 
-const DateInput: React.FC<Props> = ({ control }) => {
-  const [inputs, setInputs] = useState([{ id: nanoid(), date: '', startHour: '', endHour: '' }]);
+/**
+ * DateInput component
+ * 
+ * Used to manage and render a dynamic list of date inputs. 
+ * 
+ * It is designed to be used with the react-hook-form library and the zod schema validation library.
+ * The component uses the Controller component from react-hook-form to manage the form state of each input field.
+ * 
+ * @description 
+ * The component renders a list of date, start hour, and end hour fields based on the current state of the fieldArray
+ * Each set of fields includes a button that either adds a new set of fields or removes the current set, only the last input can add more.
+ * 
+ * @param {DateInputProps} props - Props for the component
+ * @returns {React.FC<Props>} - DateInput component
+ */
+const DateInput: React.FC<DateInputProps> = ({ control, fieldArray }) => {
+  const { fields, append: appendField, remove } = fieldArray;
+  // Use useEffect to ensure there is always at least one set of fields
+  useEffect(() => {
+    if (fields.length === 0) {
+      appendField({ date: '', startHour: '', endHour: '' });
+    }
+  }, [fields, appendField]);
 
   const addInput = () => {
-    const newId = nanoid();
-    setInputs([...inputs, { id: newId, date: '', startHour: '', endHour: '' }]);
-  };
-
-  const deleteInput = (id: string) => {
-    const newInputs = inputs.filter((input) => input.id !== id);
-    setInputs(newInputs);
-  };
-
-  const validateDate = (date: string) => {
-    const today = new Date().toISOString().split('T')[0];
-    console.log(today, date);
-    return date >= today;
-  };
-
-  const isInvalid = (value: string) => {
-    return React.useMemo(() => {
-      if (value === '') return false;
-
-      return validateDate(value) ? false : true;
-    }, [value]);
+    appendField({ date: '', startHour: '', endHour: '' });
   };
 
   return (
     <>
-      {/* {inputs.map((input, index) => {
-        const { id, date, startHour, endHour } = input;
-        const isLastInput = index === inputs.length - 1;
+      {fields.map((field, index) => {
+        const isLastInput = index === fields.length - 1;
         return (
-          <React.Fragment key={id}> */}
-      <div className="col-span-2 md:col-span-1">
-        {/* <div className="min-h-0 h-0 flex justify-end w-full">
-          <Button
-            size="sm"
-            isIconOnly
-            radius="full"
-            onClick={isLastInput ? addInput : () => deleteInput(id)}
-            className={`translate-x-4 translate-y-2 text-white z-50  ${
-              isLastInput ? 'bg-gray-300 hover:bg-primary-light' : 'bg-red-200 hover:bg-red-700'
-            }`}
-          >
-            {isLastInput ? '+' : '-'}
-          </Button>
-        </div> */}
-        <Controller
-          name={`dates`}
-          control={control}
-          rules={{ required: true }}
-          shouldUnregister={true}
-          render={({ field, fieldState, formState }) => {
-            return (
-              <Input
-                isInvalid={!!formState.errors?.[`dates`]?.message}
-                errorMessage={formState.errors?.[`dates`]?.message?.toString()}
-                radius="sm"
-                type="date"
-                label={`Fecha `}
-                placeholder="Fecha"
-                labelPlacement="outside"
-                classNames={{ base: 'col-span-2 md:col-span-1' }}
-                value={field.value}
-                onChange={field.onChange}
+          <React.Fragment key={field.id}>
+            <div className="col-span-2 md:col-span-1">
+              <div className="min-h-0 h-0 flex justify-end w-full">
+                <Button
+                  size="sm"
+                  isIconOnly
+                  radius="full"
+                  onClick={isLastInput ? addInput : () => remove(index)}
+                  className={`translate-x-4 translate-y-2 text-white z-50  ${isLastInput ? 'bg-gray-300 hover:bg-primary-light' : 'bg-red-200 hover:bg-red-700'
+                    }`}
+                >
+                  {isLastInput ? '+' : '-'}
+                </Button>
+              </div>
+              <Controller
+                name={`dates.${index}.date`}
+                control={control}
+                rules={{ required: true }}
+                shouldUnregister={true}
+                render={({ field, fieldState, formState }) => {
+                  const errorMessage = formState.errors?.dates?.[index]?.date?.message;
+
+                  return (
+                    <Input
+                      isInvalid={!!errorMessage}
+                      errorMessage={errorMessage?.toString()}
+                      radius="sm"
+                      type="date"
+                      label={`Fecha ${index + 1}`}
+                      placeholder="Fecha"
+                      labelPlacement="outside"
+                      classNames={{ base: 'col-span-2 md:col-span-1' }}
+                      value={field.value}
+                      onChange={field.onChange}
+                    />
+                  );
+                }}
               />
-            );
-          }}
-        />
-      </div>
-      <Controller
-        name={`startHours`}
-        control={control}
-        rules={{ required: true }}
-        shouldUnregister={true}
-        render={({ field, fieldState, formState }) => {
-          return (
-            <Input
-              isInvalid={!!formState.errors?.[`startHours`]?.message}
-              errorMessage={formState.errors?.[`startHours`]?.message?.toString()}
-              radius="sm"
-              type="time"
-              label={`Hora de inicio `}
-              placeholder="Hora de inicio"
-              labelPlacement="outside"
-              classNames={{ base: 'col-span-2 md:col-span-1' }}
-              value={field.value}
-              onChange={field.onChange}
+            </div>
+            <Controller
+              name={`dates.${index}.startHour`}
+              control={control}
+              rules={{ required: true }}
+              shouldUnregister={true}
+              render={({ field, fieldState, formState }) => {
+                const errorMessage = formState.errors?.dates?.[index]?.startHour?.message;
+                return (
+                  <Input
+                    isInvalid={!!errorMessage}
+                    errorMessage={errorMessage?.toString()}
+                    radius="sm"
+                    type="time"
+                    label={`Hora de inicio ${index + 1}`}
+                    placeholder="Hora de inicio"
+                    labelPlacement="outside"
+                    classNames={{ base: 'col-span-2 md:col-span-1' }}
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                );
+              }}
             />
-          );
-        }}
-      />
-      <Controller
-        name={`endHours`}
-        control={control}
-        rules={{ required: true }}
-        shouldUnregister={true}
-        render={({ field, fieldState, formState }) => {
-          return (
-            <Input
-              isInvalid={!!formState.errors?.[`endHours`]?.message}
-              errorMessage={formState.errors?.[`endHours`]?.message?.toString()}
-              radius="sm"
-              label={`Hora de cierre `}
-              placeholder="Hora de cierre"
-              labelPlacement="outside"
-              classNames={{ base: 'col-span-2 md:col-span-1' }}
-              type="time"
-              value={field.value}
-              onChange={field.onChange}
+            <Controller
+              name={`dates.${index}.endHour`}
+              control={control}
+              rules={{ required: true }}
+              shouldUnregister={true}
+
+              render={({ field, fieldState, formState }) => {
+                const errorMessage = formState.errors?.dates?.[index]?.endHour?.message;
+                return (
+                  <Input
+                    isInvalid={!!errorMessage}
+                    errorMessage={errorMessage?.toString()}
+                    radius="sm"
+                    label={`Hora de cierre ${index + 1} `}
+                    placeholder="Hora de cierre"
+                    labelPlacement="outside"
+                    classNames={{ base: 'col-span-2 md:col-span-1' }}
+                    type="time"
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                );
+              }}
             />
-          );
-        }}
-      />
-      {/* </React.Fragment>
+          </React.Fragment>
         );
-      })} */}
+      })}
     </>
   );
 };
