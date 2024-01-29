329ee9c2cc80e7e14be0bd8c40d31740a4620f99
Kevin Bravo <bravokevinto@gmail.com>
2024-01-29 01:58:11 -0400

    fix: solve erros with undefined values

--- src/lib/calendar/calendarDescription.ts
+++ src/lib/calendar/calendarDescription.ts
@@ -1,68 +1,44 @@
-import { parseModalityFromDatabase, parseSkillFromDatabase } from '@/lib/utils2';
+import { parseChatLevelFromDatabase, parseModalityFromDatabase, parseSkillFromDatabase } from '@/lib/utils2';
 import { IChatCalendar, IWorkshopCalendar } from './d';
-
-const createWorkshopCalendarDescription = (workshop: IWorkshopCalendar,
-  meetingLink?: string,
-  meetingId?: string,
-  meetingPassword?: string
-): string => {
-  const {
-    modality,
-    year,
-    platform,
-    asociated_skill,
-    description,
-    speakersData
-  } = workshop
-  const speakersNames = speakersData.map(speaker => speaker.speakerName)
-  const calendarDescription = `<b>Modalidad:</b> ${parseModalityFromDatabase(modality)}
-<b>Año del taller:</b> ${year.join(', ')}
-${speakersData.length === 1 ? `<b>Facilitador:</b> ${speakersNames[0]}` : `<b>Facilitadores:</b> ${speakersNames.join(', ')}`}
-${modality === 'ONLINE' ? `<b>Plataforma:</b> ${platform}` : `<b>Lugar:</b> ${platform}`}
-<b>Competencia Asociada:</b> ${parseSkillFromDatabase(asociated_skill)}
-${meetingLink ?? `<b>Link de la reunion:</b> ${meetingLink}`}
-${meetingId ?? `<b>Id de la reunion:</b> ${meetingId}`}
-${meetingPassword ?? `<b>Contraseña de la reunion:</b> ${meetingPassword}`}
-
-${description ?? description}
-`
-  return calendarDescription;
-}
-
-const createChatCalendarDescription = (chat: IChatCalendar,
-  meetingLink?: string,
-  meetingId?: string,
-  meetingPassword?: string): string => {
-  const {
-    level,
-    speakersData,
-    modality,
-    platform,
-    description } = chat
-  const speakersNames = speakersData.map(speaker => speaker.speakerName)
-
-  let calendarDescription = `<b>Nivel:</b> ${level} 
-${speakersData.length === 1 ? `<b>Facilitador:</b> ${speakersNames[0]}` : `<b>Facilitadores:</b> ${speakersNames.join(', ')}`}
-<b>Modalidad:</b> ${parseModalityFromDatabase(modality)}
-${modality === 'ONLINE'
-      ? `<b>Plataforma:</b> ${platform}`
-      : `<b>Lugar:</b> ${platform}`
-    }
-    ${meetingLink ?? `<b>Link de la reunion:</b> ${meetingLink}`}
-    ${meetingId ?? `<b>Id de la reunion:</b> ${meetingId}`}
-    ${meetingPassword ?? `<b>Contraseña de la reunion:</b> ${meetingPassword}`}
-    
-    ${description ?? description}
-    `
-  return calendarDescription;
-};
 
 const createCalendarDescription = (activity: IChatCalendar | IWorkshopCalendar, meetingLink?: string,
   meetingId?: string,
   meetingPassword?: string) => {
-  if ('asociated_skill' in activity) return createWorkshopCalendarDescription(activity, meetingLink, meetingId, meetingPassword)
-  else if ('level' in activity) return createChatCalendarDescription(activity, meetingLink, meetingId, meetingPassword)
-  else return ''
+  const {
+    modality,
+    platform,
+    description,
+    speakersData
+  } = activity
+  const speakersNames = speakersData.map(speaker => speaker.speakerName)
+
+  let calendarCommonDescription = `<b>Modalidad:</b> ${parseModalityFromDatabase(modality)}
+${speakersData.length === 1 ? `<b>Facilitador:</b> ${speakersNames[0]}` : `<b>Facilitadores:</b> ${speakersNames.join(', ')}`}
+${modality === 'ONLINE' ? `<b>Plataforma:</b> ${platform}` : `<b>Lugar:</b> ${platform}`}
+`
+  if ('asociated_skill' in activity) {
+    const {
+      year,
+      asociated_skill,
+    } = activity
+    calendarCommonDescription += `<b>Año del taller:</b> ${year.join(', ')}
+    <b>Competencia Asociada:</b> ${parseSkillFromDatabase(asociated_skill)}
+`
+  } else if ('level' in activity) {
+    const { level } = activity
+    calendarCommonDescription += `<b>Nivel:</b> ${parseChatLevelFromDatabase(level)} 
+`
+  }
+  if (modality === 'ONLINE') {
+    calendarCommonDescription += `<b>Link de la reunion:</b> ${meetingLink}
+<b>Id de la reunion:</b> ${meetingId}
+${platform === 'ZOOM' ? `<b>Contraseña de la reunion:</b> ${meetingPassword}` : ''}
+`
+  }
+
+
+  return calendarCommonDescription += `
+${description}`;
 }
 
 export default createCalendarDescription;
