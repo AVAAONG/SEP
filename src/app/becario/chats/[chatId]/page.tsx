884c3faf3e988711e3f8a2e66b16b688005c4b5e
Kevin Bravo <bravokevinto@gmail.com>
2024-02-05 09:47:13 -0400

    fix: error in chat when not enroled

--- src/app/becario/chats/[chatId]/page.tsx
+++ src/app/becario/chats/[chatId]/page.tsx
@@ -17,29 +17,31 @@ const page = async ({ params }: { params: { chatId: shortUUID.SUUID } }) => {
   const se = await getServerSession(authOptions);
   const chatId = params.chatId || ('null' as shortUUID.SUUID);
   const attendance = await getChatWithSpecificScholarAttendance(chatId, se?.scholarId);
-  const scholars = await getNotEnrolledScholarsInChat(chatId)
+  const scholars = await getNotEnrolledScholarsInChat(chatId);
   const { chat } = attendance || {};
   const chatForSpeaker = await getChat(chatId);
   const isDisabled = () => {
-    if (attendance?.attendance! !== 'ENROLLED') return true
-    else if (new Date(chat?.start_dates![0]!) <= new Date()) return true
-    else if (chat?.activity_status !== 'SENT') return true
-    else if (chat.speaker[0].id === se?.scholarId) return true
-    else return false
-  }
+    if (attendance?.attendance! !== 'ENROLLED') return true;
+    else if (new Date(chat?.start_dates![0]!) <= new Date()) return true;
+    else if (chat?.activity_status !== 'SENT') return true;
+    else if (chat.speaker[0].id === se?.scholarId) return true;
+    else return false;
+  };
   const scholarAttendanceDataForTable = formatScholarDataForAttendanceTable(
-    chatForSpeaker?.scholar_attendance ? chatForSpeaker.scholar_attendance.map((a) => a.scholar.scholar) : [],
+    chatForSpeaker?.scholar_attendance
+      ? chatForSpeaker.scholar_attendance.map((a) => a.scholar.scholar)
+      : [],
     chatForSpeaker?.scholar_attendance ? chatForSpeaker.scholar_attendance : []
   );
 
   return (
     <div className="min-h-screen flex flex-col gap-4">
-      <ActivityPanelInfo activity={chat as ChatWithSpeaker}  >
+      <ActivityPanelInfo activity={chatForSpeaker as ChatWithSpeaker}>
         {chat?.speaker[0].id === se?.scholarId ? (
           <></>
         ) : (
-          <div className='w-full flex gap-4  items-center justify-end'>
-            <div className='flex gap-2 items-center justify-center'>
+          <div className="w-full flex gap-4  items-center justify-end">
+            <div className="flex gap-2 items-center justify-center">
               <h3 className=" leading-none tracking-tight text-primary-light font-semibold">
                 Estatus de asistencia
               </h3>
@@ -47,25 +49,29 @@ const page = async ({ params }: { params: { chatId: shortUUID.SUUID } }) => {
                 <ScholarAttendanceWidget value={attendance?.attendance!} />
               </div>
             </div>
-            <ActivityScholarActions activityId={chatId} attendanceId={attendance?.id!} kindOfActivity='chat' scholars={scholars} isButtonDisabled={isDisabled()} />
+            <ActivityScholarActions
+              activityId={chatId}
+              attendanceId={attendance?.id!}
+              kindOfActivity="chat"
+              scholars={scholars}
+              isButtonDisabled={isDisabled()}
+            />
           </div>
-        )
-        }
-      </ActivityPanelInfo >
-      {
-        chat?.speaker[0].id === se?.scholarId ? (
-          <section className="w-full space-y-3">
-            <h2 className="px-8 text-2xl leading-none tracking-tight text-primary-light font-semibold">
-              Becarios
-            </h2>
-            <div className="flex flex-row items-center space-x-2">
-              <div className="overflow-x-scroll md:overflow-x-clip rounded-lg w-full">
-                <Table
-                  tableColumns={ScholarActivityAttendance}
-                  tableData={scholarAttendanceDataForTable}
-                  tableHeadersForSearch={[]}
-                >
-                  {/* <ExportButton
+        )}
+      </ActivityPanelInfo>
+      {chat?.speaker[0].id === se?.scholarId ? (
+        <section className="w-full space-y-3">
+          <h2 className="px-8 text-2xl leading-none tracking-tight text-primary-light font-semibold">
+            Becarios
+          </h2>
+          <div className="flex flex-row items-center space-x-2">
+            <div className="overflow-x-scroll md:overflow-x-clip rounded-lg w-full">
+              <Table
+                tableColumns={ScholarActivityAttendance}
+                tableData={scholarAttendanceDataForTable}
+                tableHeadersForSearch={[]}
+              >
+                {/* <ExportButton
                   activityTitle={chat.title!}
                   competenceOrLevel={parseChatLevelFromDatabase(chat.level!)}
                   date={chat.start_dates ? new Date(chat.start_dates[0]).toLocaleDateString('ez-VE') : ''}
@@ -85,28 +91,27 @@ const page = async ({ params }: { params: { chatId: shortUUID.SUUID } }) => {
                   }
                   attendeesData={scholarDataToExport}
                 /> */}
-                </Table>
-              </div>
+              </Table>
             </div>
-          </section>
-        ) : (
-          <section className="w-full space-y-3">
-            <h2 className="px-8 text-2xl leading-none tracking-tight text-primary-light font-semibold">
-              Becarios inscritos
-            </h2>
-            <div className="flex flex-row items-center space-x-2">
-              <div className="overflow-x-scroll md:overflow-x-clip rounded-lg w-full">
-                <Table
-                  tableColumns={ScholarActivityAttendanceForScholarTemp}
-                  tableData={scholarAttendanceDataForTable}
-                  tableHeadersForSearch={[]}
-                />
-              </div>
+          </div>
+        </section>
+      ) : (
+        <section className="w-full space-y-3">
+          <h2 className="px-8 text-2xl leading-none tracking-tight text-primary-light font-semibold">
+            Becarios inscritos
+          </h2>
+          <div className="flex flex-row items-center space-x-2">
+            <div className="overflow-x-scroll md:overflow-x-clip rounded-lg w-full">
+              <Table
+                tableColumns={ScholarActivityAttendanceForScholarTemp}
+                tableData={scholarAttendanceDataForTable}
+                tableHeadersForSearch={[]}
+              />
             </div>
-          </section>
-        )
-      }
-    </div >
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
