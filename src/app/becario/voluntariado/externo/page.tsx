e528ecbfadb44aa4f1cf0f00a12611500c7337d7
Kevin Bravo <bravokevinto@gmail.com>
2024-02-06 18:09:35 -0400

    refactor: external volunter page

--- src/app/becario/voluntariado/externo/page.tsx
+++ src/app/becario/voluntariado/externo/page.tsx
@@ -1,71 +1,267 @@
 'use client';
-import { useForm } from 'react-hook-form';
+import { MODALITY } from '@/lib/constants';
+import externalVolunteerSubmisionSchema from '@/lib/schemas/scholar/externalVolunteerSubmisionSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Input, Select, SelectItem, Textarea } from '@nextui-org/react';
+import { BaseSyntheticEvent } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { z } from 'zod';
 
 const page = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting, isValid },
+  } = useForm<z.infer<typeof externalVolunteerSubmisionSchema>>({
+    resolver: zodResolver(externalVolunteerSubmisionSchema),
+  });
+
+  const submitExternalVolunter = async (
+    data: z.infer<typeof externalVolunteerSubmisionSchema>,
+    event: BaseSyntheticEvent<object, any, any> | undefined
+  ) => {
+    event?.preventDefault();
+    console.log(data);
+  };
   return (
-    <section className="flex flex-col px-2 pt-6  justify-start items-center w-full gap-4 h-screen">
+    <section className="flex flex-col p-12 justify-start items-center w-full gap-4">
       <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white mb-4">
         ¡Sube tu voluntariado externo!
       </h1>
-      <form className="flex justify-center">
-        <div className="grid grid-cols-7 gap-4 w-full justify-center">
-          <div className="col-span-6 ">
-            <label
-              htmlFor="studyArea"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Lugar donde realizó la actividad
-            </label>
-            <input type="text" placeholder="" required className="bg-white" />
-          </div>
-          <div className="col-span-6 ">
-            <label
-              htmlFor="currentAcademicPeriod"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Fecha de la actividad{' '}
-            </label>
-            <input type="date" required className="bg-white" />
-          </div>
-          <div className="col-span-6 ">
-            <label
-              htmlFor="grade"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Indique el número de horas dedicadas a la actividad.
-            </label>
-            <input type="number" required className="bg-white" />
-          </div>
-          <div className="col-span-6 ">
-            <label
-              htmlFor="grade"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Nombre de la organización, empresa o institución beneficiada
-            </label>
-            <input type="text" required className="bg-white" />
-          </div>
-          <div className="col-span-6 ">
-            <label
-              htmlFor="grade"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Nombre del beneficiario o supervisor responsable
-            </label>
-            <input type="text" required className="bg-white" />
-          </div>
-          <div className="col-span-6 ">
-            <label
-              htmlFor="carrer"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Describa brevemente la actividad realizada
-            </label>
-            <textarea required className="bg-white" />
-          </div>
+      <form
+        onSubmit={handleSubmit((data, event) =>
+          toast.promise(submitExternalVolunter(data, event), {
+            pending: 'Subiendo voluntariado...',
+            success: 'voluntariado enviado con éxito',
+            error: 'Ocurrió un error alsubir el voluntariado',
+          })
+        )}
+        className="flex flex-col lg:flex-row gap-8 justify-center w-full"
+      >
+        <div className="grid grid-cols-2 gap-4 w-full justify-center">
+          <Controller
+            name="title"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['title']?.message}
+                  errorMessage={formState.errors?.['title']?.message?.toString()}
+                  isRequired
+                  type="text"
+                  label="Titulo de la actividad"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  radius="sm"
+                  labelPlacement="outside"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="modality"
+            control={control}
+            rules={{ required: true }}
+            shouldUnregister={true}
+            render={({ field, formState }) => {
+              return (
+                <Select
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['modality']?.message}
+                  errorMessage={formState.errors?.['modality']?.message?.toString()}
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  radius="sm"
+                  label="Modalidad"
+                  labelPlacement="outside"
+                >
+                  {MODALITY.map((modality) => (
+                    <SelectItem key={modality.value} value={modality.value}>
+                      {modality.label}
+                    </SelectItem>
+                  ))}
+                </Select>
+              );
+            }}
+          />
+          <Controller
+            name="start_dates"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['start_dates']?.message}
+                  errorMessage={formState.errors?.['start_dates']?.message?.toString()}
+                  isRequired
+                  type="date"
+                  label="Fecha de inicio"
+                  radius="sm"
+                  labelPlacement="outside"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  placeholder="Fecha de inicio de la actividad"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="end_dates"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['end_dates']?.message}
+                  errorMessage={formState.errors?.['end_dates']?.message?.toString()}
+                  isRequired
+                  type="date"
+                  label="Fecha de cierre"
+                  radius="sm"
+                  labelPlacement="outside"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  placeholder="Fecha de cierre de la actividad"
+                />
+              );
+            }}
+          />
+
+          <Controller
+            name="place"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['place']?.message}
+                  errorMessage={formState.errors?.['place']?.message?.toString()}
+                  isRequired
+                  type="text"
+                  label="Lugar donde se realizo la actividad"
+                  radius="sm"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  labelPlacement="outside"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="beneficiary"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['beneficiary']?.message}
+                  errorMessage={formState.errors?.['beneficiary']?.message?.toString()}
+                  isRequired
+                  type="text"
+                  label="Beneficiario"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  radius="sm"
+                  labelPlacement="outside"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="supervisor"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['supervisor']?.message}
+                  errorMessage={formState.errors?.['supervisor']?.message?.toString()}
+                  isRequired
+                  type="text"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  label="Supervisor de la actividad"
+                  radius="sm"
+                  labelPlacement="outside"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="supervisor_email"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['supervisor_email']?.message}
+                  errorMessage={formState.errors?.['supervisor_email']?.message?.toString()}
+                  isRequired
+                  type="text"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  label="Correo del supervisor"
+                  radius="sm"
+                  labelPlacement="outside"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="asigned_hours"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, formState }) => {
+              return (
+                <Input
+                  value={field.value?.toString()}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['asigned_hours']?.message}
+                  errorMessage={formState.errors?.['asigned_hours']?.message?.toString()}
+                  isRequired
+                  type="number"
+                  classNames={{ base: 'col-span-2 md:col-span-1' }}
+                  label="Horas asignadas"
+                  radius="sm"
+                  labelPlacement="outside"
+                />
+              );
+            }}
+          />
+          <Controller
+            name="description"
+            control={control}
+            rules={{ required: true }}
+            shouldUnregister={true}
+            render={({ field, formState }) => {
+              return (
+                <Textarea
+                  radius="sm"
+                  value={field.value || undefined}
+                  onChange={field.onChange}
+                  isInvalid={!!formState.errors?.['description']?.message}
+                  errorMessage={formState.errors?.['description']?.message?.toString()}
+                  label="Descripción"
+                  labelPlacement="outside"
+                  classNames={{
+                    base: 'col-span-2 h-fit w-full',
+                  }}
+                  placeholder="(Opcional) Coloca la descripcion de la actividad"
+                />
+              );
+            }}
+          />
         </div>
-        <div className="flex w-full h-full">
+        <div className="flex w-full lg:w-2/3 justify-center ">
           <label
             htmlFor="dropzone-file"
             className=" p-4 flex flex-col items-center justify-center w-full h-full border-2 border-green-300 border-dashed rounded-lg cursor-pointer bg-green-50 dark:hover:bg-bray-800 dark:bg-emerald-950 hover:bg-green-100 dark:border-emerald-900 dark:hover:border-emerald-800 dark:hover:bg-emerald-900"
@@ -92,19 +288,35 @@ const page = () => {
               </p>
               <p className="text-xs text-green-500 dark:text-green-400">
                 {' '}
-                PNG, JPG o PDF (MAX. 800x400px)
+                Solo archivos en formato PDF
               </p>
             </div>
-            <input id="dropzone-file" type="file" className="hidden" />
+            <Controller
+              name="proof"
+              control={control}
+              rules={{ required: true }}
+              render={({ field, formState }) => {
+                return (
+                  <input
+                    value={field.value}
+                    onChange={field.onChange}
+                    errorMessage={formState.errors?.['proof']?.message?.toString()}
+                    id="dropzone-file"
+                    type="file"
+                    className="hidden"
+                  />
+                );
+              }}
+            />
           </label>
         </div>
       </form>
-      <button
+      <Button
         className="text-white bg-green-600 hover:bg-green-500 hover:text-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         type="submit"
       >
         Subir voluntariado
-      </button>
+      </Button>
     </section>
   );
 };
---
+++ src/lib/schemas/scholar/externalVolunteerSubmisionSchema.ts
@@ -0,0 +1,44 @@
+import { Modality } from '@prisma/client';
+import { z } from 'zod';
+const MAX_FILE_SIZE = 5000000; // 5MB
+
+const externalVolunteerSubmisionSchema = z
+    .object({
+        title: z.string().min(1, { message: 'Necesitas colocar un titulo' }),
+        start_dates: z.string()
+            .min(1, { message: 'Debes especificar la fecha' })
+            .refine((date) => new Date(date) >= new Date(), {
+                message: 'La fecha no puede ser menor a la actual',
+            }),
+        end_dates: z.string()
+            .min(1, { message: 'Debes especificar la fecha' })
+            .refine((date) => new Date(date) >= new Date(), {
+                message: 'La fecha no puede ser menor a la actual',
+            }),
+        modality: z.nativeEnum(Modality, {
+            errorMap: (issue, _ctx) => {
+                switch (issue.code) {
+                    case 'invalid_enum_value':
+                        return { message: 'Debes seleccionar una modalidad valida' };
+                    default:
+                        return { message: 'Debes seleccionar la modalidad' };
+                }
+            },
+        }),
+        description: z.string().min(1, { message: 'Debes especificar la descripción' }),
+        place: z.string().min(1, { message: 'Debes especificar el lugar' }),
+        beneficiary: z.string().min(1, { message: 'Debes especificar el beneficiario' }),
+        supervisor: z.string().min(1, { message: 'Debes especificar el beneficiario' }),
+        supervisor_email: z.string().min(1, { message: 'Debes especificar el beneficiario' }),
+        proof: z.instanceof(File)
+            .refine((file) => file?.size <= MAX_FILE_SIZE, 'File size should be less than 5MB.')
+            .refine((file) => {
+                const acceptedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+                return acceptedTypes.includes(file?.type);
+            }, 'Only these types are allowed: .jpg, .jpeg, .png, and .pdf'),
+        asigned_hours: z.number().min(1, { message: 'Debes especificar las horas asignadas' }),
+    });
+
+
+
+export default externalVolunteerSubmisionSchema;
