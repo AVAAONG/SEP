688768d375ca029d4963c95853097bbd6bdcb065
Kevin Bravo <bravokevinto@gmail.com>
2024-02-08 12:18:22 -0400

    fix: attributes errors

--- public/svgs/SocialNetworks.tsx
+++ public/svgs/SocialNetworks.tsx
@@ -182,9 +182,9 @@ export const YoutubeIcon = () => (
 );
 
 export const TiktokIcon = () => (
-  <svg fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+  <svg fill="currentColor" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
       {' '}
       <title>tiktok</title>{' '}
