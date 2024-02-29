0d179b4a87f7a876d3589e1a2fb3e6ecba7e00ef
Kevin Bravo <bravokevinto@gmail.com>
2024-02-29 15:22:22 -0400

    feat: allow date filtering

--- src/app/admin/voluntariado/externo/page.tsx
+++ src/app/admin/voluntariado/externo/page.tsx
@@ -1,11 +1,19 @@
+import DateSelector from '@/components/commons/datePicker';
 import Table from '@/components/table/Table';
 import ExternalVolunteerAdminColumns from '@/components/table/columns/adminVolunteerColumns';
 import { getExternalVolunteer } from '@/lib/db/utils/volunteer';
+import filterActivitiesBySearchParams from '@/lib/utils/datePickerFilters';
 import { parseKindOfVolunteerFromDatabase, parseModalityFromDatabase } from '@/lib/utils2';
 
-const page = async () => {
+const page = async ({
+  searchParams,
+}: {
+  searchParams?: { year: string; month: string; quarter: string };
+}) => {
   const volunteerData = await getExternalVolunteer();
-  const v = volunteerData.map((volunteer) => {
+
+  const volunteers = filterActivitiesBySearchParams(volunteerData, searchParams);
+  const v = volunteers.map((volunteer) => {
     return {
       scholarNames:
         volunteer.volunteer_attendance[0].scholar.scholar.first_names.split(' ')[0] +
@@ -24,11 +32,12 @@ const page = async () => {
     };
   });
   return (
-    <div>
+    <div className="min-h-screen flex flex-col gap-6  items-center">
+      <DateSelector />
       <div className="flex flex-col px-2 pt-6 xl:gap-4">
         <div className="mb-4 col-span-full xl:mb-2">
           <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
-            Registro de actividades de voluntariado
+            Registro de actividades de voluntariado externas
           </h1>
           <div className="h-full max-w-7xl flex flex-col gap-4 pt-4"></div>
         </div>
