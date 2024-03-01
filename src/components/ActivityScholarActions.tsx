e36af3ea03129ba2bb5ef921ed04bfb63063e8e5
Kevin Bravo <bravokevinto@gmail.com>
2024-03-01 14:36:26 -0400

    feat: godd forgot to add constrains

--- src/components/ActivityScholarActions.tsx
+++ src/components/ActivityScholarActions.tsx
@@ -136,7 +136,7 @@ const ActivityScholarActions: React.FC<ActivityPanelInfoProps> = ({
   return (
     ///TODO set isDisable option
     <div className="w-1/2 flex items-center justify-end">
-      <Button onPress={onOpen} color="warning" className="text-white" isDisabled={false}>
+      <Button onPress={onOpen} color="warning" className="text-white" isDisabled={isButtonDisabled}>
         Cancelar inscripcion
       </Button>
       <BasicModal
