3e3b8b45c292c09086190075f7c56443034dad94
Kevin Bravo <bravokevinto@gmail.com>
2024-03-06 17:25:49 -0400

    refactor: html spot transfer message

--- src/components/ActivityScholarActions.tsx
+++ src/components/ActivityScholarActions.tsx
@@ -1,77 +1,12 @@
 'use client';
+import createTransferSpotMessage from '@/lib/htmls/transferSpotMessage';
 import { sendGenericEmail } from '@/lib/sendEmails';
-import { parseModalityFromDatabase } from '@/lib/utils2';
 import { Autocomplete, AutocompleteItem, Avatar, Button, useDisclosure } from '@nextui-org/react';
-import { Modality, Scholar } from '@prisma/client';
+import { Scholar } from '@prisma/client';
 import { useMemo, useState } from 'react';
 import { toast } from 'react-toastify';
 import BasicModal from './BasicModal';
 import ScholarActivitySatisfactionSurvey from './ScholarActivitySatisfactionSurvey';
-
-const createCeaseConfirmationMessage = (
-  scholarName: string,
-  scholarWhoCeaseName: string,
-  activityName: string,
-  date: string,
-  startDate: string | Date,
-  endDate: string | Date,
-  modality: string,
-  platform: string,
-  link: string
-) => {
-  return `<table>
-  <tr><td>Hola, ${scholarName}</td></tr>
-
-  <tr><td style="height: 20px;"></td></tr>
-
-  <tr><td>${scholarWhoCeaseName} te cedió el cupo a la actividad de ${activityName}</td></tr>
-    
-  <tr><td style="height: 20px;"></td></tr>
-
-  <tr><td style="font-weight: bold;">Detalles de la actividad:</td></tr>
-
-  <tr><td>Fecha: ${new Date(date).toLocaleDateString('es-ES', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-  })}</td></tr>
-
-  <tr><td>Hora: De ${new Date(startDate).toLocaleTimeString('es-ES', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true,
-  })} a ${new Date(endDate).toLocaleTimeString('es-ES', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true,
-  })}</td></tr>
-
-  <tr><td>Modalidad: ${parseModalityFromDatabase(modality as Modality)}</td></tr>
-
-  <tr><td>Plataforma: ${platform}</td></tr>
-  
-  <tr><td style="height: 20px;"></td></tr>
-
-  <tr><td>Para confirmar tu asistencia haz clic en el siguiente enlace: ${link}</td></tr>
-    
-  <tr><td style="height: 20px;"></td></tr>
-
-  <tr><td>👀 Recuerda, inscribirse es un compromiso que adquieres con AVAA y contigo mismo.</td></tr>
-
-  <tr><td>Al inscribirte, te comprometes a asistir a la actividad y cumplir con las responsabilidades y obligaciones que se te asignen.</td></tr>
-    
-  <tr><td style="height: 20px;"></td></tr>
-
-  <tr><td>Importante:</td></tr>
-
-  <tr><td>Si no has recibido información previa sobre esta actividad por parte de ${scholarWhoCeaseName}, NO hagas clic en el enlace.</td></tr>
-
-  <tr><td style="height: 20px;"></td></tr>
-
-  <tr><td>¡Esperamos tu participación!</td></tr>
-</table>
-`;
-};
 
 interface ActivityPanelInfoProps {
   scholars: Scholar[];
@@ -122,7 +57,7 @@ const ActivityScholarActions: React.FC<ActivityPanelInfoProps> = ({
       selectedScholar.first_names.split(' ')[0] || ''
     )}&eventId=${encodeURIComponent(eventId)}&activityName=${encodeURIComponent(activityName)}`;
 
-    const message = createCeaseConfirmationMessage(
+    const message = createTransferSpotMessage(
       selectedScholar.first_names.split(' ')[0] || '',
       scholarWhoCeaseName,
       activityName,
