511003b2d305ca2074d379df1c8778f576067435
Kevin Bravo <bravokevinto@gmail.com>
2024-03-06 17:19:10 -0400

    refactor: delete unnused google api files

--- src/app/becario/universidad/page.tsx
+++ src/app/becario/universidad/page.tsx
@@ -1,5 +1,49 @@
-const page = () => {
-  return <div>page</div>;
+import AddCvaModule from '@/components/AddCvaModule';
+import ScholarCVAInformation from '@/components/forms/scholarCVAInfo';
+import Table from '@/components/table/Table';
+import CvaModulesColumns from '@/components/table/columns/cvaModuleColumns';
+import authOptions from '@/lib/auth/nextAuthScholarOptions/authOptions';
+import { getBlobFile } from '@/lib/azure/azure';
+import { getCvaInformationByScholar } from '@/lib/db/utils/cva';
+import { parseCvaScheduleFromDatabase, parseModalityFromDatabase } from '@/lib/utils2';
+import { getServerSession } from 'next-auth';
+
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  const cvaInformation = await getCvaInformationByScholar(session?.scholarId!);
+  const cvaCertificate = await getBlobFile(cvaInformation?.certificate);
+  const cvaModules = await Promise.all(
+    cvaInformation?.modules.map(async (module) => {
+      return {
+        ...module,
+        modality: parseModalityFromDatabase(module?.modality),
+        schedule: parseCvaScheduleFromDatabase(module.schedule),
+        record: module.record ? await getBlobFile(module.record) : null,
+      };
+    }) || []
+  );
+
+  return (
+    <div className="flex flex-col pt-6 gap-4 min-h-screen">
+      <h1 className="text-xl font-semibold  sm:text-2xl ">Información sobre el CVA</h1>
+      <div className="p-2 lg:p-6 w-full bg-gray-100 dark:bg-black rounded-lg">
+        <ScholarCVAInformation
+          scholarCvaInformation={cvaInformation}
+          certificateUrl={cvaCertificate}
+          scholarId={session?.scholarId!}
+        />
+      </div>
+      <div className="w-full">
+        <Table
+          tableData={cvaModules || []}
+          tableColumns={CvaModulesColumns}
+          tableHeadersForSearch={[]}
+        >
+          <AddCvaModule cvaInformationId={cvaInformation?.id || null} />
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default page;
