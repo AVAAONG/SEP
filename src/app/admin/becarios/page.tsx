023f2141f49443f1fc5ba59be17978cce0267023
Kevin Bravo <bravokevinto@gmail.com>
2024-10-21 21:51:47 -0400

    fix: total scholars count

--- src/app/admin/becarios/page.tsx
+++ src/app/admin/becarios/page.tsx
@@ -32,21 +32,21 @@ const page = async ({
   const scholars = await getScholarsWithAllData();
   const scholarsPropertiesCount = countScholarGeneralProperties(scholars);
 
-  const activateScholars = scholars.filter((scholar) => scholar.program_information?.scholar_condition === 'ACTIVE');
-  
+  const activateScholars = scholars.filter(
+    (scholar) => scholar.program_information?.scholar_condition === 'ACTIVE'
+  );
 
-  const activeScholars = scholarsPropertiesCount.condition.ACTIVE
+  const activeScholars = scholarsPropertiesCount.condition.ACTIVE;
 
   const probationI = Number(
-    ((scholarsPropertiesCount.status.PROBATION_I /activeScholars) * 100).toFixed(0)
+    ((scholarsPropertiesCount.status.PROBATION_I / activeScholars) * 100).toFixed(0)
   );
   const probationII = Number(
-    ((scholarsPropertiesCount.status.PROBATION_II /activeScholars) * 100).toFixed(0)
+    ((scholarsPropertiesCount.status.PROBATION_II / activeScholars) * 100).toFixed(0)
   );
   const toBeAlumniComparation = Number(
-    ((scholarsPropertiesCount.condition.TO_BE_ALUMNI /activeScholars) * 100).toFixed(0)
+    ((scholarsPropertiesCount.condition.TO_BE_ALUMNI / activeScholars) * 100).toFixed(0)
   );
-
 
   return (
     <div className="flex flex-col w-full gap-4">
@@ -54,7 +54,7 @@ const page = async ({
         stats={[
           {
             name: `Becarios activos`,
-            stat:activeScholars || 0,
+            stat: activeScholars || 0,
             changeType: 'increase',
             comparationText: null,
             tooltipText: null,
@@ -63,7 +63,7 @@ const page = async ({
             name: `Becarios en espera de egreso`,
             stat: scholarsPropertiesCount.condition.TO_BE_ALUMNI,
             changeType: 'increase',
-            comparationText: `De ${activeScholars || 0} becarios activos`,
+            comparationText: `De ${activeScholars + scholarsPropertiesCount.condition.TO_BE_ALUMNI || 0} becarios`,
             comparation: toBeAlumniComparation,
             tooltipText: `0% de los becar ios se encuentran proximos a egresar`,
           },
