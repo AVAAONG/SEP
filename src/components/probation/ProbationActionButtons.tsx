73a7d2364fe193592be6f48ebe7b71ef252fe953
Kevin Bravo <bravokevinto@gmail.com>
2024-11-19 15:10:41 -0400

    feat: s

--- src/components/probation/ProbationActionButtons.tsx
+++ src/components/probation/ProbationActionButtons.tsx
@@ -8,7 +8,6 @@ import { Probation, ScholarStatus } from '@prisma/client';
 import { toast } from 'react-toastify';
 import ProbationForm from '../ProbationForm';
 
-
 interface ProbationAccordionProps {
   scholarData: {
     id: string;
@@ -17,8 +16,7 @@ interface ProbationAccordionProps {
     surNames: string;
     dni: string;
   };
-  probation: Probation
-
+  probation: Probation;
 }
 
 const ProbationActionButtons: React.FC<ProbationAccordionProps> = ({ probation, scholarData }) => {
@@ -29,15 +27,16 @@ const ProbationActionButtons: React.FC<ProbationAccordionProps> = ({ probation, 
     openConfirmation(
       {
         title: 'Confirmar eliminacion',
-        message: '¿Está seguro que desea borrar este estatus de probatorio? Esta acción no se puede deshacer.',
+        message:
+          '¿Está seguro que desea borrar este estatus de probatorio? Esta acción no se puede deshacer.',
         confirmText: 'Borrar',
-        cancelText: 'Cancelar'
+        cancelText: 'Cancelar',
       },
-      async () => toast.promise(deleteProbation(probation.id, scholarData.id),
-        {
+      async () =>
+        toast.promise(deleteProbation(probation.id, scholarData.id), {
           error: 'Error al eliminar informacion de probatorio',
           pending: 'Eliminando informacion de probatorio',
-          success: 'Informacion de probatorio eliminada de forma exitosa'
+          success: 'Informacion de probatorio eliminada de forma exitosa',
         })
     );
   };
@@ -47,21 +46,20 @@ const ProbationActionButtons: React.FC<ProbationAccordionProps> = ({ probation, 
         title: 'Confirmar accion',
         message: '¿Está seguro que desea terminar el estatus de probatorio de este becario?.',
         confirmText: 'Quitar estatus',
-        cancelText: 'Cancelar'
+        cancelText: 'Cancelar',
       },
-      async () => toast.promise(endScholarProbation(probation.id, scholarData.id),
-        {
+      async () =>
+        toast.promise(endScholarProbation(probation.id, scholarData.id), {
           error: 'Error al terminar estatus de probatorio',
           pending: 'Terminando estatus de probatorio',
-          success: 'Probatorio terminado de forma exitosa'
+          success: 'Probatorio terminado de forma exitosa',
         })
     );
   };
   return (
     <>
-
       <Button
-        isDisabled={probation.ending_date !== null}
+        // isDisabled={probation.ending_date !== null}
         radius="sm"
         onPress={() =>
           toast.promise(
@@ -71,7 +69,7 @@ const ProbationActionButtons: React.FC<ProbationAccordionProps> = ({ probation, 
                 last_names: scholarData.surNames,
                 dni: scholarData.dni,
               },
-              probation,
+              probation
             ),
             {
               pending: 'Creando acta...',
@@ -85,15 +83,29 @@ const ProbationActionButtons: React.FC<ProbationAccordionProps> = ({ probation, 
       >
         Crear acta
       </Button>
-      <Button onPress={handleDelete}
-        color={probation.kind_of_probation === 'PROBATION_I' ? 'warning' : 'danger'} radius="sm" variant="ghost">
+      <Button
+        onPress={handleDelete}
+        color={probation.kind_of_probation === 'PROBATION_I' ? 'warning' : 'danger'}
+        radius="sm"
+        variant="ghost"
+      >
         Borrar
       </Button>
-      <Button onPress={modalActions.onOpen} color={probation.kind_of_probation === 'PROBATION_I' ? 'warning' : 'danger'} radius="sm" variant="ghost">
+      <Button
+        onPress={modalActions.onOpen}
+        color={probation.kind_of_probation === 'PROBATION_I' ? 'warning' : 'danger'}
+        radius="sm"
+        variant="ghost"
+      >
         Editar
       </Button>
-      <Button isDisabled={probation.ending_date !== null} onPress={handleEndScholarProbation}
-        color={probation.kind_of_probation === 'PROBATION_I' ? 'warning' : 'danger'} radius="sm" variant="ghost">
+      <Button
+        isDisabled={probation.ending_date !== null}
+        onPress={handleEndScholarProbation}
+        color={probation.kind_of_probation === 'PROBATION_I' ? 'warning' : 'danger'}
+        radius="sm"
+        variant="ghost"
+      >
         Quitar estatus de probatorio
       </Button>
       <ConfirmationModal />
