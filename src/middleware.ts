89462e2f5932bd05d60351c66914f0f5fba1bc85
Kevin Bravo <bravokevinto@gmail.com>
2025-01-31 11:14:01 -0400

    fix: add debug to middleware and quit root path handlig

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,43 +10,46 @@ const ACCESS_DENIED_PATH = '/accessDenied';
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET }) as Token | null;
+
+  console.log('Debug:', { pathname, tokenExists: !!token, role: token?.role });
+
   // Redirect to /signin if no token is present
   if (!token && pathname !== '/signin') {
     return NextResponse.redirect(new URL('/signin', req.url));
   }
 
-  if (pathname === '/') return NextResponse.redirect(new URL('/signin', req.url))
-
   // Protect routes
   if (pathname.startsWith('/admin')) {
     if (!token || token.role !== 'ADMIN') {
+      console.log('Admin access denied. Role:', token?.role);
+
       return NextResponse.redirect(new URL(ACCESS_DENIED_PATH, req.url));
     }
   }
 
   if (pathname.startsWith('/becario')) {
     if (!token || (token.role !== 'SCHOLAR')) {
+      console.log('Scholar access denied. Role:', token?.role);
+
       return NextResponse.redirect(new URL(ACCESS_DENIED_PATH, req.url));
     }
   }
 
   // Handle post-authentication redirects
-  if (pathname === '/signin') {
-    if (token) {
-      if (token.role === 'ADMIN') {
-        return NextResponse.redirect(new URL('/admin/panel', req.url));
-      }
-      else if (token.role === 'SCHOLAR') {
-        return NextResponse.redirect(new URL('/becario/panel', req.url));
-      }
-      else {
-        return NextResponse.redirect(new URL(ACCESS_DENIED_PATH, req.url));
-      }
+  if (pathname === '/signin' && token) {
+    if (token.role === 'ADMIN') {
+      return NextResponse.redirect(new URL('/admin/panel', req.url));
+    }
+    else if (token.role === 'SCHOLAR') {
+      return NextResponse.redirect(new URL('/becario/panel', req.url));
+    }
+    else {
+      return NextResponse.redirect(new URL(ACCESS_DENIED_PATH, req.url));
     }
   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/admin/:path*', '/becario/:path*', '/signin', '/'],
+  matcher: ['/admin/:path*', '/becario/:path*', '/signin'],
 };
