83692216564032fa1afb28d9306df4001d0cee80
Kevin Bravo <bravokevinto@gmail.com>
2025-01-31 16:25:18 -0400

    feat: improve styles, perf and functions

--- src/app/captacion/postulacion/layout.tsx
+++ src/app/captacion/postulacion/layout.tsx
@@ -1,66 +1,13 @@
 import { SidebarNav } from '@/components/catchment/SidebarPostulationForm';
-import {
-  AcademicCapIcon,
-  BriefcaseIcon,
-  GlobeAltIcon,
-  UserIcon,
-} from '@heroicons/react/24/outline';
+
 import { Divider } from '@nextui-org/react';
 
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Postulación al programa Excelencia',
-  description: 'Formulario de postulación al programa de excelencia',
+  title: 'Formulario de postulación',
+  description: 'Formulario de postulación al Programa Excelencia AVAA',
 };
-
-const sidebarNavItems = [
-  {
-    title: 'Datos personales',
-    href: '/captacion/postulacion',
-    icon: <UserIcon className="w-4" />,
-  },
-  {
-    title: 'Datos de contacto',
-    href: '/captacion/postulacion/contacto',
-    icon: <AcademicCapIcon className="w-4" />,
-  },
-  {
-    title: 'Datos familiares',
-    href: '/captacion/postulacion/familia',
-    icon: <BriefcaseIcon className="w-4" />,
-  },
-  {
-    title: 'Situación laboral',
-    href: '/captacion/postulacion/laboral',
-    icon: <GlobeAltIcon className="w-4" />,
-  },
-  {
-    title: 'Conocimiento de idiomas',
-    href: '/captacion/postulacion/idiomas',
-    icon: <GlobeAltIcon className="w-4" />,
-  },
-  {
-    title: 'Educación secundaria',
-    href: '/captacion/postulacion/secundaria',
-    icon: <GlobeAltIcon className="w-4" />,
-  },
-  {
-    title: 'Educación universitaria',
-    href: '/captacion/postulacion/universidad',
-    icon: <GlobeAltIcon className="w-4" />,
-  },
-  {
-    title: 'Información adicional',
-    href: '/captacion/postulacion/adicional',
-    icon: <GlobeAltIcon className="w-4" />,
-  },
-  {
-    title: 'Anexos',
-    href: '/captacion/postulacion/anexos',
-    icon: <GlobeAltIcon className="w-4" />,
-  },
-];
 
 interface SettingsLayoutProps {
   children: React.ReactNode;
@@ -75,7 +22,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
       <Divider />
       <div className="flex flex-col lg:flex-row gap-4 md:gap-10 ">
         <aside className="lg:w-1/6 lg:h-full mt-1">
-          <SidebarNav items={sidebarNavItems} />
+          <SidebarNav currentStep={9} />
         </aside>
         <div className="flex-1 lg:w-full">{children}</div>
       </div>
--- src/components/catchment/SidebarPostulationForm.tsx
+++ src/components/catchment/SidebarPostulationForm.tsx
@@ -1,51 +1,125 @@
 'use client';
+import {
+  AcademicCapIcon,
+  Bars4Icon,
+  BriefcaseIcon,
+  BuildingLibraryIcon,
+  BuildingOfficeIcon,
+  ClipboardDocumentListIcon,
+  LanguageIcon,
+  UserGroupIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline';
 
 import { useMediaQuery } from '@/hooks/use-media-query';
-import { Button, Divider, Link, Select } from '@nextui-org/react';
+import { Button, Link, Select, SelectItem } from '@nextui-org/react';
 
 import { usePathname, useRouter } from 'next/navigation';
 
-interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
-  items: {
-    href: string;
-    title: string;
-    icon: React.ReactNode;
-  }[];
-}
+const SIDEBAR_NAV_ITEMS = [
+  {
+    title: 'Datos personales',
+    href: '/captacion/postulacion',
+    icon: <UserIcon className="w-5" />,
+    step: 1,
+  },
+  {
+    title: 'Datos de contacto',
+    href: '/captacion/postulacion/contacto',
+    icon: <AcademicCapIcon className="w-5" />,
+    step: 2,
+  },
+  {
+    title: 'Datos familiares',
+    href: '/captacion/postulacion/familia',
+    icon: <UserGroupIcon className="w-5" />,
+    step: 3,
+  },
+  {
+    title: 'Situación laboral',
+    href: '/captacion/postulacion/laboral',
+    icon: <BriefcaseIcon className="w-5" />,
+    step: 4,
+  },
+  {
+    title: 'Conocimiento de idiomas',
+    href: '/captacion/postulacion/idiomas',
+    icon: <LanguageIcon className="w-5" />,
+    step: 5,
+  },
+  {
+    title: 'Educación secundaria',
+    href: '/captacion/postulacion/secundaria',
+    icon: <BuildingOfficeIcon className="w-5" />,
+    step: 6,
+  },
+  {
+    title: 'Educación universitaria',
+    href: '/captacion/postulacion/universidad',
+    icon: <BuildingLibraryIcon className="w-5" />,
+    step: 7,
+  },
+  {
+    title: 'Información adicional',
+    href: '/captacion/postulacion/adicional',
+    icon: <Bars4Icon className="w-5" />,
+    step: 8,
+  },
+  {
+    title: 'Anexos',
+    href: '/captacion/postulacion/anexos',
+    icon: <ClipboardDocumentListIcon className="w-5" />,
+    step: 9,
+  },
+];
 
-export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
+export function SidebarNav({ currentStep }: { currentStep: number }) {
   const pathname = usePathname();
   const isDesktop = useMediaQuery('(min-width: 1024px)');
   const router = useRouter();
 
-  const handleChange = (value: string) => router.push(value);
+  const disabledSteps = SIDEBAR_NAV_ITEMS.filter((item) => item.step > currentStep).map(
+    (item) => item.href
+  );
 
+  const handleChange = (value: string) => router.push(value);
   if (!isDesktop) {
     return (
       <div className="w-full">
         <Select
+          items={SIDEBAR_NAV_ITEMS}
+          disabledKeys={disabledSteps}
+          renderValue={(items) => {
+            return items.map((item) => (
+              <div className="flex items-center space-x-2">
+                {item.data?.icon} <p>{item.data?.title}</p>
+              </div>
+            ));
+          }}
           radius="sm"
           className="!w-full"
-          defaultValue={pathname}
-          onChange={handleChange}
-          options={items.map((item) => ({
-            value: item.href,
-            label: item.title,
-          }))}
-        />
-        <Divider />
+          defaultSelectedKeys={[pathname]}
+          onChange={(event) => handleChange(event.target.value)}
+        >
+          {(item) => (
+            <SelectItem key={item.href} startContent={item.icon}>
+              {item.title}
+            </SelectItem>
+          )}
+        </Select>
       </div>
     );
   }
 
   return (
-    <nav className={'flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1'} {...props}>
-      {items.map((item) => (
+    <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
+      {SIDEBAR_NAV_ITEMS.map((item) => (
         <Button
           radius="sm"
           size="md"
           color="success"
           as={Link}
+          isDisabled={item.step > currentStep}
           key={item.href}
           href={item.href}
           startContent={item.icon}
