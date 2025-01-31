e03291a6051f344e727663af5204dd73657521a8
Kevin Bravo <bravokevinto@gmail.com>
2025-01-31 00:51:03 -0400

    feat: handle new session object

--- src/app/becario/layout.tsx
+++ src/app/becario/layout.tsx
@@ -2,8 +2,6 @@ import MainLayout from '@/components/scholar/MainLayout';
 import NavigationBar from '@/components/scholar/NavigationBar';
 import Sidebar from '@/components/scholar/Sidebar';
 import { getServerSession } from '@/lib/auth/authOptions';
-import { getBlobImage } from '@/lib/azure/azure';
-import { prisma } from '@/lib/db/utils/prisma';
 
 export default async function RootLayout({
   children,
@@ -12,31 +10,19 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession();
-  const image = await prisma.scholar.findUnique({
-    where: {
-      id: session?.id,
-    },
-    select: {
-      photo: true,
-      program_information: {
-        select: {
-          is_chat_speaker: true,
-          scholar_status: true,
-        },
-      },
-    },
-  });
+  if (!session) return null;
+  const { image, name, email, scholarStatus, id, isSpeaker } = session;
   return (
     <>
       <div className="antialiased bg-white dark:bg-black">
         <NavigationBar
-          image={await getBlobImage(image?.photo)}
-          name={session?.user?.name}
-          email={session?.user?.email}
-          scholarStatus={image?.program_information?.scholar_status}
-          scholarId={session?.id}
+          image={image}
+          name={name}
+          email={email}
+          scholarStatus={scholarStatus}
+          scholarId={id}
         />
-        <Sidebar isSpeaker={image?.program_information?.is_chat_speaker} />
+        <Sidebar isSpeaker={isSpeaker} />
         <MainLayout>{children}</MainLayout>
       </div>
     </>
