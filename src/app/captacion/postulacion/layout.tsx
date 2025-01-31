d2048fe4191dd5f548d9c066a5877e7c569fcf52
Kevin Bravo <bravokevinto@gmail.com>
2025-01-31 12:53:52 -0400

    style: improve background

--- src/app/captacion/postulacion/layout.tsx
+++ src/app/captacion/postulacion/layout.tsx
@@ -68,12 +68,12 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
-    <main className="p-10 min-h-screen flex flex-col space-y-4">
+    <main className="p-10 min-h-screen flex flex-col space-y-4 bg-gray-100">
       <h1 className="text-3xl md:text-4xl font-bold text-primary-light">
         Formulario de postulación ProExcelencia
       </h1>
       <Divider />
-      <div className="flex flex-col lg:flex-row gap-6 ">
+      <div className="flex flex-col lg:flex-row gap-4 md:gap-10 ">
         <aside className="lg:w-1/6 lg:h-full mt-1">
           <SidebarNav items={sidebarNavItems} />
         </aside>
