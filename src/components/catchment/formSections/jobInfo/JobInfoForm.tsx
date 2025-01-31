fa796db0081709052f0f3aecf9d65f69c5354e14
Kevin Bravo <bravokevinto@gmail.com>
2025-01-31 13:10:22 -0400

    feat: add job info

---
+++ src/components/catchment/formSections/jobInfo/JobInfoForm.tsx
@@ -0,0 +1,120 @@
+'use client';
+import InputField from '@/components/fields/InputFormField';
+import SelectFormField from '@/components/fields/SelectFormField';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Link } from '@nextui-org/react';
+import { FormProvider, useForm, useWatch } from 'react-hook-form';
+import { z } from 'zod';
+import jobInfoSchema from './jobInfoSchema';
+
+type JobFormSchemaType = z.infer<typeof jobInfoSchema>;
+
+const JobInfoForm = () => {
+  const methods = useForm<JobFormSchemaType>({
+    resolver: zodResolver(jobInfoSchema),
+    mode: 'all',
+  });
+  const onSubmit = (data: JobFormSchemaType) => {
+    console.log(data);
+    methods.reset(
+      {},
+      {
+        keepErrors: false,
+      }
+    );
+  };
+
+  const itsWorking = useWatch({
+    control: methods.control,
+    name: 'currently_working',
+  });
+
+  const itsWorkingBoolean = (itsWorking as unknown as string) === 'YES';
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full space-y-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+          <SelectFormField
+            isRequired
+            label="¿Actualmente trabaja?"
+            name="currently_working"
+            selectItems={[
+              { label: 'Si', value: 'YES' },
+              { label: 'No', value: 'NO' },
+            ]}
+          />
+          <InputField
+            isRequired={itsWorkingBoolean}
+            isDisabled={!itsWorkingBoolean}
+            type="text"
+            label="Nombre de la organización/empresa donde trabaja"
+            name="job_company"
+          />
+          <InputField
+            isRequired={itsWorkingBoolean}
+            isDisabled={!itsWorkingBoolean}
+            type="text"
+            label="Cargo que desempeña"
+            name="job_title"
+          />
+          <SelectFormField
+            isRequired={itsWorkingBoolean}
+            isDisabled={!itsWorkingBoolean}
+            label="Modalidad de trabajo"
+            name="job_modality"
+            selectItems={[
+              {
+                label: 'Prsencial',
+                value: 'IN_PERSON',
+              },
+              {
+                label: 'Virtual',
+                value: 'ONLINE',
+              },
+              {
+                label: 'Mixta',
+                value: 'HYBRID',
+              },
+            ]}
+          />
+          <SelectFormField
+            isRequired={itsWorkingBoolean}
+            isDisabled={!itsWorkingBoolean}
+            label="Horario de trabajo"
+            name="job_schedule"
+            selectItems={[
+              {
+                label: 'Tiempo completo',
+                value: 'FULL_TIME',
+              },
+              {
+                label: 'Tiempo parcial',
+                value: 'PART_TIME',
+              },
+              {
+                label: 'Fines de semana',
+                value: 'WEEKENDS',
+              },
+            ]}
+          />
+        </div>
+        <div className="col-span-2 flex gap-4">
+          <Button
+            radius="sm"
+            as={Link}
+            href="/captacion/familia"
+            className="w-full"
+            variant="bordered"
+          >
+            Anterior
+          </Button>
+          <Button radius="sm" type="submit" className="w-full">
+            Siguiente
+          </Button>
+        </div>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default JobInfoForm;
