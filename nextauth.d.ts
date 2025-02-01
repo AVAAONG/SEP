1ff102be46e9cda41055bf03035457bcb04d0331
Kevin Bravo <bravokevinto@gmail.com>
2025-01-31 21:06:47 -0400

    feat: support for new kind of user

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -1,10 +1,10 @@
-import { ScholarStatus } from "@prisma/client";
+import { KinOfUser, ScholarStatus } from "@prisma/client";
 import "next-auth";
 
 /// @see https://reacthustle.com/blog/extend-user-session-nextauth-typescript?expand_article=1
 declare module "next-auth" {
     interface User extends DefaultUser {
-        kind_of_user: 'SCHOLAR' | 'ADMIN';
+        kind_of_user: KinOfUser;
     }
     interface Session extends DefaultSession {
         kindOfUser: 'SCHOLAR' | 'ADMIN';
