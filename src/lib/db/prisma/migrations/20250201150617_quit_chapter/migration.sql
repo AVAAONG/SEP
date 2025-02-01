16ea80e6a42c4d771cd3e6798af8d32fd4a13bd2
Kevin Bravo <bravokevinto@gmail.com>
2025-02-01 11:08:37 -0400

    fix: quit chapter id from schema

---
+++ src/lib/db/prisma/migrations/20250201150617_quit_chapter/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `chapterId` on the `personal_info` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "personal_info" DROP COLUMN "chapterId";
