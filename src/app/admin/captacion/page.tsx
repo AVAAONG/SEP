5caebe2d707a0282ed8b32937621c5a13ac39a47
Kevin Bravo <bravokevinto@gmail.com>
2025-02-26 15:03:42 -0400

    feat: allow to reach files

--- src/app/admin/captacion/page.tsx
+++ src/app/admin/captacion/page.tsx
@@ -1,6 +1,6 @@
 import CombinedColumns from '@/components/table/columns/applicant/applicants';
 import Table from '@/components/table/Table';
-import { getBlobImage } from '@/lib/azure/azure';
+import { getBlobFile, getBlobImage } from '@/lib/azure/azure';
 import { getApplicantsWithAllInfo } from '@/lib/db/utils/applicant';
 const flattenApplicantData = (applicant) => {
   return {
@@ -21,8 +21,23 @@ const page = async () => {
   const applicants = await getApplicantsWithAllInfo();
   const flattenedApplicants = applicants.map(flattenApplicantData);
   await Promise.all(
-    flattenedApplicants.map(async (applicant: { photo: string | null }) => {
+    flattenedApplicants.map(async (applicant: any) => {
       applicant.photo = await getBlobImage(applicant.photo);
+      applicant.annexes.dniCard = await getBlobFile(applicant.annexes.dniCard);
+      applicant.annexes.rif = await getBlobFile(applicant.annexes.rif);
+      applicant.annexes.highSchoolGrades = await getBlobFile(applicant.annexes.highSchoolGrades);
+      applicant.annexes.universityGrades = await getBlobFile(applicant.annexes.universityGrades);
+      applicant.annexes.studyProof = await getBlobFile(applicant.annexes.studyProof);
+      applicant.annexes.professorReferenceLetterI = await getBlobFile(
+        applicant.annexes.professorReferenceLetterI
+      );
+      applicant.annexes.professorReferenceLetterII = await getBlobFile(
+        applicant.annexes.professorReferenceLetterII
+      );
+      applicant.annexes.utilityBillVerification = await getBlobFile(
+        applicant.annexes.utilityBillVerification
+      );
+      applicant.annexes.personalEssay = await getBlobFile(applicant.annexes.personalEssay);
     })
   );
 
