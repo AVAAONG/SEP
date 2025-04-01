92d9fda8cfbb4a4f09d40a87ce11ccc99726b0fe
Kevin Bravo <bravokevinto@gmail.com>
2025-04-01 16:39:10 -0400

    fix: missgender

--- src/components/table/columns/widgets/gender-ship.tsx
+++ src/components/table/columns/widgets/gender-ship.tsx
@@ -10,7 +10,7 @@ export const GenderShip = ({ gender }: { gender: Gender }) => {
   } else if (gender === 'F') {
     return (
       <span className="inline-flex items-center bg-rose-100 text-rose-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-rose-900 dark:text-rose-300">
-        Empresa
+        Femenino
       </span>
     );
   } else if (gender === 'O') {
