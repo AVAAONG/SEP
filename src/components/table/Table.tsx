763cc6a2a85e15adc37e79e07328ae0aa3f6d06c
Kevin Bravo <bravokevinto@gmail.com>
2025-07-13 12:43:35 -0400

    feat: improve expanded state

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -94,98 +94,122 @@ function Table<T extends Record<string, unknown>>({
   }, [state.sortBy]);
 
   return (
-    <div
-      className={`${
-        isExpanded
-          ? 'absolute top-0 left-0 right-0 z-50 min-h-screen'
-          : 'relative overflow-hidden min-h-max shadow-emerald-600 dark:bg-slate-900 sm:rounded-lg w-full'
-      }  bg-white shadow-md `}
-    >
-      <div className="flex flex-col px-4 py-3 gap-3 md:flex-row md:items-center md:justify-between md:space-y-0 ">
-        <TableSearchButton
-          optionsForFilter={tableHeadersForSearch}
-          setFilter={setFilter}
-          filterValue={globalFilter}
+    <>
+      {isExpanded && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-300 ease-in-out"
+          onClick={() => toggleExpanded(false)}
         />
-        <div className="flex gap-3">
-          {children && children}
-          <HideColumnsButton columns={allColumns} />
-          <Button
-            variant="flat"
-            size="sm"
-            onPress={async () => {
-              const exportData = data.map((row) => processRow(row, preprocessedColumns));
-              await exportDataToExcel(exportData, 'Reporte');
-            }}
-            startContent={<ArrowUpTrayIcon className="w-4 h-4 text-primary-1" />}
-          >
-            <span className="hidden sm:inline">Exportar</span>
-          </Button>
-          <ExpandTableButton isExpanded={isExpanded} toggleExpanded={toggleExpanded} />
+      )}
+      <div
+        className={`${
+          isExpanded
+            ? 'fixed inset-8 z-50 bg-white shadow-lg rounded-lg overflow-auto max-h-[calc(100vh-4rem)] transition-all duration-300 ease-in-out transform'
+            : 'relative overflow-hidden min-h-max shadow-emerald-600 dark:bg-slate-900 sm:rounded-lg w-full'
+        } bg-white shadow-md`}
+      >
+        <div className="flex flex-col px-4 py-3 gap-3 md:flex-row md:items-center md:justify-between md:space-y-0 ">
+          <TableSearchButton
+            optionsForFilter={tableHeadersForSearch}
+            setFilter={setFilter}
+            filterValue={globalFilter}
+          />
+          <div className="flex gap-3">
+            {children && children}
+            <HideColumnsButton columns={allColumns} />
+            <Button
+              variant="flat"
+              size="sm"
+              onPress={async () => {
+                const exportData = data.map((row) => processRow(row, preprocessedColumns));
+                await exportDataToExcel(exportData, 'Reporte');
+              }}
+              startContent={<ArrowUpTrayIcon className="w-4 h-4 text-primary-1" />}
+            >
+              <span className="hidden sm:inline">Exportar</span>
+            </Button>
+            <ExpandTableButton isExpanded={isExpanded} toggleExpanded={toggleExpanded} />
+          </div>
         </div>
-      </div>
 
-      <div className="flow-root w-full overflow-x-scroll">
-        <table {...getTableProps()} className="w-full  text-sm text-left text-gray-300 ">
-          <thead className="text-xs text-primary-light  text-center border-b-[1px] border-primary-light text-ellipsis  dark:bg-slate-950">
-            {headerGroups.map((headerGroup) => {
-              const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps();
-              return (
-                <tr
-                  key={key}
-                  {...restHeaderGroupProps}
-                  className=" text-xs  text-primary-light dark:text-primary-dark uppercase tracking-wide font-light"
-                >
-                  {headerGroup.headers.map((column) => {
-                    const { getHeaderProps, getSortByToggleProps } = column;
-                    const { key, ...restColumn } = getHeaderProps(getSortByToggleProps());
-                    return (
-                      <th key={key} {...restColumn} scope="col" className="px-2">
-                        <div className="flex text-center py-1.5 gap-1.5 justify-center items-center">
-                          {column.render('Header')}
-                          {column.disableSortBy ? null : column.isSorted ? (
-                            column.isSortedDesc ? (
-                              <SortIconReverse />
+        <div className="flow-root w-full overflow-x-scroll">
+          <table {...getTableProps()} className="w-full  text-sm text-left text-gray-300 ">
+            <thead className="sticky top-0 z-10 text-xs text-primary-light text-center border-b-[1px] border-primary-light text-ellipsis dark:bg-slate-950">
+              {headerGroups.map((headerGroup) => {
+                const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps();
+                return (
+                  <tr
+                    key={key}
+                    {...restHeaderGroupProps}
+                    className="text-xs text-primary-light dark:text-primary-dark uppercase tracking-wide font-light"
+                  >
+                    {headerGroup.headers.map((column, i) => {
+                      const { getHeaderProps, getSortByToggleProps } = column;
+                      const { key, ...restColumn } = getHeaderProps(getSortByToggleProps());
+                      return (
+                        <th
+                          key={key}
+                          {...restColumn}
+                          scope="col"
+                          className={`px-2 ${
+                            i === 0 ? 'sticky left-0 bg-white dark:bg-slate-950 z-20' : ''
+                          }`}
+                        >
+                          <div className="flex text-center py-1.5 gap-1.5 justify-center items-center">
+                            {column.render('Header')}
+                            {column.disableSortBy ? null : column.isSorted ? (
+                              column.isSortedDesc ? (
+                                <SortIconReverse />
+                              ) : (
+                                <SortIconDown />
+                              )
                             ) : (
-                              <SortIconDown />
-                            )
-                          ) : (
-                            <SortIcon />
-                          )}
-                        </div>
-                      </th>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </thead>
-          <tbody {...getTableBodyProps()} className="divide-y divide-gray-300 dark:divide-gray-800">
-            {allowPagination(isExpanded).map((row) => {
-              prepareRow(row);
-              const { key, ...restRowProps } = row.getRowProps();
-              return (
-                <tr
-                  key={key}
-                  {...restRowProps}
-                  className="text-sm hover:bg-secondary-2 dark:hover:bg-secondary-dark dark:hover:text-white text-center text-gray-800 dark:text-gray-300"
-                >
-                  {row.cells.map((cell) => {
-                    const { key, ...restCellProps } = cell.getCellProps();
-                    return (
-                      <td key={key} {...restCellProps} className="px-4 py-1 whitespace-nowrap">
-                        {cell.render('Cell')}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                              <SortIcon />
+                            )}
+                          </div>
+                        </th>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </thead>
+            <tbody
+              {...getTableBodyProps()}
+              className="divide-y divide-gray-300 dark:divide-gray-800"
+            >
+              {allowPagination(isExpanded).map((row) => {
+                prepareRow(row);
+                const { key, ...restRowProps } = row.getRowProps();
+                return (
+                  <tr
+                    key={key}
+                    {...restRowProps}
+                    className="text-sm hover:bg-secondary-2 dark:hover:bg-secondary-dark dark:hover:text-white text-center text-gray-800 dark:text-gray-300"
+                  >
+                    {row.cells.map((cell, i) => {
+                      const { key, ...restCellProps } = cell.getCellProps();
+                      return (
+                        <td
+                          key={key}
+                          {...restCellProps}
+                          className={`px-4 py-1 whitespace-nowrap ${
+                            i === 0 ? 'sticky left-0 bg-white dark:bg-slate-900 z-10' : ''
+                          }`}
+                        >
+                          {cell.render('Cell')}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        {isExpanded ? null : <TableFooter table={table} />}
       </div>
-      {isExpanded ? null : <TableFooter table={table} />}
-    </div>
+    </>
   );
 }
 
