05fd78080c550b397b6b9784bb6b23cb58fcd2e4
Kevin Bravo <bravokevinto@gmail.com>
2025-07-13 11:49:14 -0400

    feat: only allow necesary

--- src/components/table/columns/scholar/activityAttendance/workshops/searchOptions.ts
+++ src/components/table/columns/scholar/activityAttendance/workshops/searchOptions.ts
@@ -1,23 +1,7 @@
 const scholarWorkshopAttendanceSearchOptions = [
     {
-        label: 'Año de la actividad',
-        option: 'year',
-    },
-    {
-        label: 'Competencia',
-        option: 'skill',
-    },
-    {
-        label: 'Modalidad',
-        option: 'modality',
-    },
-    {
-        label: 'Asistencia',
-        option: 'attendance',
-    },
-    {
-        label: 'Estatus de la actividad',
-        option: 'activityStatus',
+        label: 'Titulo',
+        option: 'title',
     },
     {
         label: 'Facilitador',
