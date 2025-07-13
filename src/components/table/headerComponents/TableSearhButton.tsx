f4fa25a435fc4de04d4a1180173f40e59cf8ed3e
Kevin Bravo <bravokevinto@gmail.com>
2025-07-13 19:23:49 -0400

    style: improve for mobile devices

--- src/components/table/headerComponents/TableSearhButton.tsx
+++ src/components/table/headerComponents/TableSearhButton.tsx
@@ -1,4 +1,5 @@
 'use client';
+import useMobile from '@/hooks/use-mobile';
 import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { Button } from '@nextui-org/button';
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/dropdown';
@@ -19,6 +20,7 @@ const TableSearchButton = ({
   filterValue,
   columns = [],
 }: TableSearchButtonProps) => {
+  const { isMobile } = useMobile();
   // Derive IDs from columns or fallback to filter options
   {
     /*  TODO: The reason for this search is because there are some columns that have a `#` number  as their first column, we should remove that*/
@@ -47,7 +49,7 @@ const TableSearchButton = ({
     };
   }, []);
   return (
-    <div className="flex w-1/2">
+    <div className="flex w-full md:w-1/2">
       <Input
         size="sm"
         variant="bordered"
@@ -64,25 +66,28 @@ const TableSearchButton = ({
         placeholder="Buscar..."
       />
       {optionsForFilter.length > 1 && (
-        <Dropdown>
+        <Dropdown size="sm">
           <DropdownTrigger>
             <Button
               isDisabled={optionsForFilter.length < 2}
               className="rounded-l-none -ml-2"
               variant="solid"
               size="sm"
-              endContent={<ChevronDownIcon className="w-full h-full" />}
+              isIconOnly={isMobile}
+              endContent={<ChevronDownIcon className="w-5 h-5" />}
             >
-              <div className="flex flex-col  items-start text-xs">
-                Buscar
-                {selectedFilter === 'all' ? (
-                  ''
-                ) : (
-                  <span className="hidden text-xs sm:inline text-green-500">
-                    {optionsForFilter.find(({ option }) => option === selectedFilter)?.label}
-                  </span>
-                )}
-              </div>
+              {!isMobile && (
+                <div className="flex flex-col  items-start text-xs">
+                  Buscar
+                  {selectedFilter === 'all' ? (
+                    ''
+                  ) : (
+                    <span className="hidden text-xs sm:inline text-green-500">
+                      {optionsForFilter.find(({ option }) => option === selectedFilter)?.label}
+                    </span>
+                  )}
+                </div>
+              )}
             </Button>
           </DropdownTrigger>
           <DropdownMenu aria-label="Dynamic Actions" items={optionsForFilter}>
